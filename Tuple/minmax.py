9cfd888249bfd6af2276cc0b028e67289b1c1bed
Dilshad Abdulla <Dilshad.Abdulla@outlook.com>
2017-11-22 00:00:10 +0000

    Define a tupe and make some examples of tuples

---
+++ Tuple/minmax.py
@@ -0,0 +1,18 @@
+
+
+a = (11, 25, 1, 67, -9, 99, 654, 35, 7, 8, 56)
+
+def minmax(numbers):
+    return min(numbers), max(numbers)
+
+print('Using min and max method to display lowerst and heighest number from the tuple')
+print(minmax(a))
+
+lower, upper = minmax(a)
+
+print('\n', '#' * 50, '\n')
+
+print('Using lower and upper method to display lowerst and heighest number from the tuple')
+print('The lowerst number is :', lower)     # return the smallest numbers
+print('#######')
+print('The heighest number is :',upper)     # return the largerst number
---
+++ Tuple/tuple_1.py
@@ -0,0 +1,23 @@
+''' 
+	A tuple is a sequence of immutable Python objects. Tuples are sequences,
+	just like lists. The only difference is that tuples can't be changed i.e.,
+	tuples are immutable and tuples use parentheses and lists use square brackets.
+	Creating a tuple is as simple as putting different comma-separated
+	values and optionally you can put these comma-separated values between
+	parentheses also. For example:
+'''
+
+tup1 = ('physics', 'chemistry', 1997, 2000)
+tup2 = (1, 2, 3, 4, 5 )
+tup3 = "a", "b", "c", "d"
+tup4 = ()
+
+print(tup1)
+print(tup2)
+print(tup3)
+print(tup4)
+
+print('#############')
+print(tup1[1])
+print(tup2[3])
+print(tup3[0])
