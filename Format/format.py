c0145ba6d92af7455e0009238670e97610e51a8d
Dilshad Abdulla <Dilshad.Abdulla@outlook.com>
2017-11-29 01:13:37 +0000

    Use python format and read file or attribute type

---
+++ Format/format.py
@@ -0,0 +1,12 @@
+msg = "Raffi's is {0} years old and he is born in {1}".format(2, 'London.')
+print(msg)
+
+location = "Current postcode is {first}{second}".format(first='E14', second=' 7FX')
+print(location)
+
+pos = (40, 175, '01/03/1975')
+
+test = 'Dilshad is a={pos[0]} years old, he is b={pos[1]} cm tall and he is born at c={pos[2]}.'.format(pos=pos)
+
+print(test)
+
