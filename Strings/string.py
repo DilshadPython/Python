9a7e65352a7b33d9268b8b934814aa487dbf2681
Dilshad Abdulla <Dilshad.Abdulla@outlook.com>
2017-12-04 14:20:30 +0000

    Test string and show that string is immutable

---
+++ Strings/string.py
@@ -0,0 +1,40 @@
+text = "Test that obj is (or is not) an instance of cls (which can be a class or a tuple of classes, as supported by"
+
+print(len(text))
+
+print;
+
+# indexing
+
+print(text[6])
+
+print;
+
+print(text[22])
+
+print;
+
+# slicing
+print('Display from lest to char number 37: >>> ', text[:37])
+
+print;
+
+print('Display all from left to right: >>> ', text[:])
+
+print;
+
+print('Display everything except last character: >>> ', text[:-1])
+
+print;
+
+print('Display from left step by 1>>> ', text[::1])
+print('Display from left step by 2 >>> ', text[::2])
+print('Display from left step by 3 >>> ', text[::3])
+print('Display from left step by 4 >>> ', text[::4])
+
+print;
+
+print('Dispay as reversed right to left: >>> ', text[::-1])
+print;
+
+
