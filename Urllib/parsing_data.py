e8253eeeaf2f94b88d7c53730abe8fddf3d0fe76
Dilshad Abdulla <Dilshad.Abdulla@outlook.com>
2017-12-11 19:42:26 +0000

    adding data use BeautifulSoup

---
+++ Urllib/parsing_data.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import urllib.request
+import time
+
+
+req = urllib.request.urlopen('https://github.com/DilshadGit/Python')
+
+# req = urllib.request.urlopen('http://www.findukphonenumber.com/numbers_beginning_with/0740002/')
+
+xml = BeautifulSoup(req, 'xml')
+
+for item in xml.findAll('link')[3:]:
+	url = item.text
+	news = urllib.request.urlopen(url).read()
+	
+	page = BeautifulSoup(news)
+
+	for p in page.findAll('p'):
+		print(p.text)
+
+	time.sleep(10)
