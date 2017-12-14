d5cdc92a69f25953cb5f7640b385cc621914f46b
Dilshad Abdulla <dilshad.abdulla@outlook.com>
2017-12-14 12:47:51 +0000

    Create basic logging and check the different between logging messages

---
+++ Logging/login_basic.py
@@ -0,0 +1,18 @@
+import logging
+
+
+def full_name():
+	fname = 'Dilshad'
+	lname = 'Abdulla'
+	logging.debug('Full name: ', fname, ' ', lname)
+
+def address(street, city, postcode):
+	return logging.debug('Street: {} \nCity: {} \nPost Code: {}'.format(street, city, postcode))
+
+def contact(phone, email):
+	logging.debug('Phone: {} \nEmail: {}'.format(phone, email))
+
+
+full_name()
+address('6 Ursula Gould Way', 'E14 7FX', 'London')
+contact('07825810648', 'info@gmail.com')
---
+++ Logging/warning_formatter.py
@@ -0,0 +1,26 @@
+import logging
+
+'''
+    We add another line below to change the debug level of the layout, this is 
+    camelcase. the debug after level=logging.DEBUG is an integer run in background
+    which is different than logging.DEBUG() , method which used logging.DEBUG(something)    
+'''
+logging.basicConfig(filename='output.log', level=logging.DEBUG,
+					format='%(asctime)s:%(levelname)s:%(message)s')
+
+def full_name():
+	fname = 'Tom'
+	lname = 'Alan'
+	logging.warning('Full name: ' + fname + ' ' + lname)
+
+def address(street, city, postcode):
+	return logging.warning('Street: {} \nCity: {} \nPost Code: {}'.format(street, city, postcode))
+
+def contact(phone, email):
+	logging.warning('Phone: {} \nEmail: {}'.format(phone, email))
+
+
+full_name()
+address('6 Ursula Gould Way', 'E14 7FX', 'London')
+contact('07825810648', 'info@gmail.com')
+print('\n')
