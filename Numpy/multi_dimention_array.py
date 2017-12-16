f5e97fe242c6a3a06c654d0777dad491911dec7b
Dilshad Abdulla <dilshad.abdulla@outlook.com>
2017-12-16 19:02:08 +0000

    Import Numpy how to used and create array multiarray and other methods related to Numpy package, like eye, empty, arange

---
+++ Numpy/multi_dimention_array.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+multi = np.ones([10, 10])
+
+print(multi)
+
+print('=============================================')
+empt = np.empty(5)
+
+print(empt)
+
+print('=============================================')
+empt_two = np.empty([8, 8])
+print(empt_two)
+
+print('=============================================')
+print('Identity matrix')
+print(np.eye(5))
