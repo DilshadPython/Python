8e36eba1474fc54d1b67fdb4af437d903192bc35
dilshadgit <dilshad.abdulla@outlook.com>
2017-12-19 12:16:34 +0000

    Use pandas indexing, head, tail and slicing

--- Pandas/README.md
+++ Pandas/README.md
@@ -1,5 +1,8 @@
 ## Pandas:
 
+### (This package not complete because of some issues with pandas_datareader and matplotlib
+### installation and python version)
+
 ### pandas is an open source, BSD-licensed library providing high-performance, easy-to-use data 	structures and data analysis tools for the Python programming language.
 
 ### pandas is a NUMFocus sponsored project. This will help ensure the success of development of pandas as a world-class open-source project, and makes it possible to donate to the project.
@@ -11,6 +14,11 @@
 ### Pandas build in visualization tools. 
 
 ### https://pandas.pydata.org/
+### https://matplotlib.org/users/installing.html
+### Ubuntu istall following
+#### sudo apt-get install python3-matplotlib
+#### sudo apt-get install python3-tk
+### sudo pip install pandas-datareader
 
 ### Pandas has two storage types:
 ### 1. Series is more like python list or is a single column of data frame converted to one dimention
---
+++ Pandas/head_tail_slice.py
@@ -0,0 +1,40 @@
+import pandas as pd 
+
+automobils = { 'Audi': ['A1', 'A2', 'A3', 'S3', 'A4', 'A5', 'A6', 'A7'],
+			   'BMW': ['BMW-06', 'BMW-20', 'BMW-24', 'BMW-30', 'BMW-35', 'BMW-40', 'BMW-45', 'BMW-50'],
+			   'MSD': ['MB-SLS', 'MB-CLA', 'MB-GLA', 'MB-SL', 'MB-R231', 'MB-G63', 'ML250', 'ML'],
+			   'Ford': ['F150', 'Ranger', 'Transit', 'SuperDuty', 'Expedition', 'Edge', 'FT', 'FTT'],
+			   'Nums': [44, 56, 72, 98, 2, 76, 99, 76],
+			}
+
+data_frame = pd.DataFrame(automobils)
+
+# When you call the head() only display the first 5 column
+print(data_frame.head())
+
+# display all columns
+print('\n-----------------------------------------')
+print(data_frame)
+
+# cut the last 5 column
+print('\n-----------------------------------------')
+print(data_frame.tail())
+
+# display the first three column
+print('\n-----------------------------------------')
+print(data_frame.head(3))
+
+# display the last two column
+print('\n-----------------------------------------')
+print(data_frame.tail(3))
+
+#slicing
+# display the first two column
+print('\n-----------------------------------------')
+print(data_frame['MSD'][4])
+
+print('\n-----------------------------------------')
+print(data_frame['Audi'][3])
+
+print('\n-----------------------------------------')
+print(data_frame['Nums'][5])
