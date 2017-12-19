6a41d8aa4c4bfb0b395542392d8b9124c0cdd18e
dilshadgit <dilshad.abdulla@outlook.com>
2017-12-19 14:03:29 +0000

    Read data using pandas and matplotlib from server

--- Pandas/visualize_data.py
+++ Pandas/visualize_data.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import datetime
-import pandas.io.data as wb
+# import pandas.io.data as wb
+import pandas_datareader
+from pandas_datareader import data, wb
 
 import matplotlib.pyplot as plt
 from matplotlib import style
@@ -11,6 +13,6 @@ style.use('fivethirtyeight')
 start = datetime.datetime(2012, 1, 3)
 end = datetime.datetime(2016, 12, 1)
 
-output = wb.DataReader('T', 'yahoo', start, end)
+output = data.DataReader('T', 'yahoo', start, end)
 
 print(output.head())
