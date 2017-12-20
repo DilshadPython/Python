fe65128cbc718d4a6cdeb74510dd5410b4672241
dilshadgit <dilshad.abdulla@outlook.com>
2017-12-20 12:47:59 +0000

    Define lambda how to use with int and str and what does lambda only small thing

---
+++ Lambda/lamb_sub.py
@@ -0,0 +1,11 @@
+# lambda is a small function with unknown name
+
+# Please enter a number
+num = int(input('Enter a number: ' ))
+
+# lambda take the num and calculate
+result = lambda num: num - 8
+
+# We will display the result and passing the number to the result
+print('{} - 8 = {}'.format(num, result(num)))
+print(result(num))
