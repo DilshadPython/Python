7f28f4b544ab91e2c3afa637b9959ebc6db6740e
dilshadgit <dilshad.abdulla@outlook.com>
2018-01-16 20:09:40 +0000

    Read and write the Premier League from UK retrieved from BBC sport football table

---
+++ BeautifulSoup/data_from_url.py
@@ -0,0 +1,30 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+
+url = 'http://www.bbc.co.uk/sport/football/tables'
+
+file = open('football_team.txt', 'w')
+
+
+i = 0
+
+while i < 20:
+
+	html_data = urlopen(url)
+	soup = BeautifulSoup(html_data, 'html.parser')
+
+	tableStats = soup.find('table', {'class': 'gs-o-table'})
+
+	for row in tableStats.findAll('tr')[1:]:
+		col = row.findAll('td')
+	
+		try:
+			name = col[2].a.string.strip()
+			# print(name)
+			file.write(name + '\n')
+		except:
+			pass
+	i += 20
+
+file.close
