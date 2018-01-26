991074c3d1123f82b81c5fb3163f587441d15841
dilshadgit <dilshad.abdulla@outlook.com>
2018-01-26 02:04:58 +0000

    Read csv file and display on terminal, read csv file via index number read csv file and create in new cs file read and write csv file in new csv file while deleting some col

---
+++ CsvFile/read_csv_as_dict.py
@@ -0,0 +1,14 @@
+import csv
+
+'''
+	to saperate value by using a field in csv file use delilmiter by ,
+
+'''
+
+with open('property-data.csv', 'r') as csvfile:
+	spamreader = csv.DictReader(csvfile)
+	
+	for row in spamreader:
+		# to read the index in one line use following
+		print(row)
+
---
+++ CsvFile/read_csv_line.py
@@ -0,0 +1,16 @@
+import csv
+
+'''
+	to saperate value by using a field in csv file use delilmiter by ,
+
+'''
+
+with open('property-data.csv', 'rt') as csvfile:
+	spamreader = csv.reader(csvfile)
+	# next read the 0 index first which is Address on index 6 but not printed out jump to next line
+	next(spamreader)
+	for row in spamreader:
+		# to read the index in one line use following
+		# print(row[1])
+		print(row[5])
+		
