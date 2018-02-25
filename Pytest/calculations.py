79c68e41e6486f73ac3d140b76c6c138f1d484b7
dilshadgit <dilshad.abdulla@outlook.com>
2018-02-25 14:56:26 +0000

    Create 3 methods and tested with pytest

---
+++ Pytest/calculations.py
@@ -0,0 +1,14 @@
+
+
+## create add func
+def adding(firstnumber, secondnumber):
+	total = firstnumber + secondnumber
+	return total
+
+def substrcting(firstnumber, secondnumber):
+	result = firstnumber - secondnumber
+	return result
+
+def multiply(firstnumber, secondnumber):
+	output = firstnumber * secondnumber
+	return output
---
+++ Pytest/test_calculations.py
@@ -0,0 +1,10 @@
+import calculations
+
+def test_adding():
+	assert calculations.adding(15, 9) == 24
+
+def test_substrcting():
+	assert calculations.substrcting(16, 7) == 9
+
+def test_multiply():
+	assert calculations.multiply(18, 4) == 72
