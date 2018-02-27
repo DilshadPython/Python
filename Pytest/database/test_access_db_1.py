a3533041f39022fd808987a80caa271fb664b19b
dilshadgit <dilshad.abdulla@outlook.com>
2018-02-27 13:04:04 +0000

    Access to the database and test the data in the database using fixture, teardown and setting up the server and closed down automatically all in one func

---
+++ Pytest/database/test_access_db_1.py
@@ -0,0 +1,29 @@
+from access_db import University
+
+
+def setup_module(module):
+	global conn
+	global cur 
+
+	db = University()
+	conn = db.access('server')
+	cur = conn.cursor()
+'''
+# when the setup_module run which connect to the db we need to run another 
+# method to close curs and close conn
+'''
+def teardown_module(module):
+	conn.close()
+
+
+def test_dilshad_id():	
+	id = cur.execute('select id from university_db where name=Dilshad')
+	assert id == 814747
+
+
+def test_rachel_id():
+	id = cur.execute('select id from university_db where name=Rachel')
+	assert id == 804878
+	
+
+
