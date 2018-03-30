8e6af3e674be83a74a778e1e512f1e61f0e75030
DilshadGit <dilshad.abdulla@icloud.com>
2018-03-30 14:27:14 +0100

    Check how to read value of 1, 0 in dict and False, true with float number

---
+++ INFO/riddle.py
@@ -0,0 +1,13 @@
+myopinion = {
+	True: 'yes', 
+	1: 'no', 
+	0.1: 'maybe', 
+	False: 0, 
+	1.1: 'not sure'
+}
+
+print(myopinion)
+
+print()
+for key, value in myopinion.items():
+	print(key, '', value)
