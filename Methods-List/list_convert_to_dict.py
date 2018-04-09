25055a4e1690183d087e841bf650e690fd4f8619
DilshadGit <dilshad.abdulla@icloud.com>
2018-04-10 00:06:14 +0100

    Convert list to dictionary more complex

---
+++ Methods-List/list_convert_to_dict.py
@@ -0,0 +1,35 @@
+import itertools
+
+teams = [
+	'Manchester City',
+	'Manchester United',
+	'Liverpool',
+	'Tottenham Hotspur',
+	'Chelsea',
+	'Arsenal',
+	'Burnley',
+	'Leicester City',
+	'Everton',
+	'AFC Bournemouth',
+	'Watford',
+	'Newcastle United',
+	'Brighton & Hove Albion',
+	'West Ham United',
+	'Swansea City',
+	'Huddersfield Town',
+	'Crystal Palace',
+	'Southhanpton',
+	'Stoke City',
+	'West Bromwich Albion',	
+]
+
+# create another list only int 
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9,10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+# zip both list together or pass them as iterators
+addthem_tother = zip(nums, teams)
+
+# pass them to dict function using list comprehentions instest of range use zip with slicing
+premier_league = dict([(key, value) for key, value in zip(nums[::1], teams[0::1])])
+
+print(premier_league)
