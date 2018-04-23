b857530d9c537c22eafc73849efe5af3c2cecb18
DilshadGit <dilshad.abdulla@icloud.com>
2018-04-24 00:09:37 +0100

    Introduce python keywords

---
+++ Keyword/keyword_list.py
@@ -0,0 +1,14 @@
+# https://www.programiz.com/python-programming/keyword-list
+import keyword
+
+print('List of python keywords')
+print(keyword.kwlist)
+
+# outcome
+'''
+List of python keywords
+['False', 'None', 'True', 'and', 'as', 'assert', 'break', 'class', 'continue', 'def', 'del', 
+'elif', 'else', 'except', 'finally', 'for', 'from', 'global', 'if', 'import', 'in', 'is', 
+'lambda', 'nonlocal', 'not', 'or', 'pass', 'raise', 'return', 'try', 'while', 'with', 'yield']
+'''
+
