f7ad972fe0b15a6522e9797433bf81c984859ea2
DilshadGit <dilshad.abdulla@icloud.com>
2018-05-13 12:50:33 +0100

    Define closure function and reflaction

---
+++ Function/clouser_function.py
@@ -0,0 +1,25 @@
+def main():
+	name = 'Dilshad Abdulla'
+	address = '301 Hallmark Court'
+
+	def submain():
+		print('%s %s' %(name, address))
+
+	return submain()
+
+
+main()
+
+print('###################################')
+
+def first_func(name, address):
+	detail = name + ' ' + address
+
+	def second_func():
+		print(detail)
+
+	return second_func()
+
+new_func = first_func('Tom Crus', 'London Road 12')
+
+new_func
