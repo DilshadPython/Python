8da5882235711775557d24f05c62293a41e96c90
DilshadGit <dilshad.abdulla@icloud.com>
2018-05-13 16:14:00 +0100

    write fibanoaci math using generator and devide function on numbers equal to 0

---
+++ Fibonacci/fizz_buz.py
@@ -0,0 +1,14 @@
+
+numbers_not_divid_to_zero = []
+
+for number in range(1, 200):
+	if number % 5 ==0 and number % 3 ==0:
+		print(True, ' == 3', True , ' == 5')
+	elif number % 3 == 0:
+		print(True, ' == 3', False, ' != 5')
+	elif number % 5 == 0:
+		print(True, ' == 5', ' || ', False, ' != 3')
+	else:
+		print(number)
+		numbers_not_divid_to_zero.append(number)
+		print(numbers_not_divid_to_zero)
