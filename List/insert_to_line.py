78487f8ec4f69d76dab5ac3d31e0618d75ba363b
Dilshad Abdulla <dilshad.a73@gmail.com>
2018-09-27 13:56:42 +0100

    Update insert to the index position in the list

---
+++ List/insert_to_line.py
@@ -0,0 +1,17 @@
+a = 'The new manager of Manchester united his name is '
+b = a.split()
+print 
+print(a)
+print(b)
+
+print('\n#######################\n')
+print(" The total length of all the words in (a) is '{}', you need to find out where do you want to insert the name.".format(len(a)))
+
+print('\n#######################\n')
+fruit = ['Apples', 'Orange', 'Bananas', 'Cherry']
+print(fruit)
+
+print('\n - Add limone to position two in the list. \n')
+# 2 is position in the list
+fruit.insert(2, 'Limone')
+print(fruit)
