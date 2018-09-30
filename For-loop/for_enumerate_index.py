a7fe6bc559cfec0429216eea830c93c857645f6e
Dilshad Abdulla <dilshad.a73@gmail.com>
2018-09-30 16:01:47 +0100

    Use for loop with range, enumerate and for loop only with if

---
+++ For-loop/for_enumerate_index.py
@@ -0,0 +1,7 @@
+names = ['Tom', 'Chris', 'Julia', 'Rob', 'Claudio', 'Sarah', 'Amanda']
+numbers = [1,2,3,4,5,6,7]
+
+print('###########################')
+for name in names:
+	for index, num in enumerate(numbers):
+		print(index, name, num)
