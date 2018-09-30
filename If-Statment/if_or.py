48d18ff3db0b70d5820b278a52e2f16f97c3e33e
Dilshad Abdulla <dilshad.a73@gmail.com>
2018-09-30 15:23:57 +0100

    Use if with following (is, and, ==, or, None, empty list, empty tuple, empty set, empty dic to reverse True to False and to reverse False to True).

---
+++ If-Statment/if_or.py
@@ -0,0 +1,17 @@
+
+user = 'Student'
+
+is_registered = True
+
+if user == 'Student' or is_registered:
+	print('\n  Yes the user already registered.')
+else:
+	print('\n Not register in the database')
+
+
+is_registered = False
+
+if user == 'Student' or is_registered:
+	print('\n  Yes the user already registered.')
+else:
+	print('\n The user is not register in the database')
---
+++ If-Statment/if_what.py
@@ -0,0 +1,15 @@
+
+if True:
+	print('Yes the condition was True')
+
+print('\n - If False \n')
+
+if False:
+	print('The condition not printed!')
+
+print('\n - If False Go to Else: \n')
+
+if False:
+	print('The condition not printed!')
+else:
+	print('Good Bye')
