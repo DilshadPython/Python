7513e3e99bdae8633107252c92f45d06bee4d60f
Dilshad Abdulla <dilshad.a73@gmail.com>
2018-10-10 18:52:26 +0100

    How __name__ and __main__ works

---
+++ _name_main_/_name_.py
@@ -0,0 +1,6 @@
+def hello():
+	print(f'\n The model __name__ in python means {__name__} : ')
+
+
+if __name__ == '__main__':
+	hello()
---
+++ _name_main_/yourfile.py
@@ -0,0 +1,9 @@
+# import myfile
+
+def main():
+	print(f'\n This file I run is yourfile')
+	print(f'\n The model __name__ in python means {__name__} : ')
+
+
+if __name__ == '__main__':
+	main()
