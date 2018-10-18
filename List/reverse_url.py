57f09d5a6ba29cd3649b62398a7e51cad44f9c48
Dilshad Abdulla <dilshad.a73@gmail.com>
2018-10-18 10:57:16 +0100

    How to reverse url from right to left as list, tupe, string

---
+++ List/reverse_url.py
@@ -0,0 +1,14 @@
+
+myweb = 'https//google.co.uk'
+
+print(myweb[::])
+
+print()
+print(myweb[::-1])
+print(myweb[::-2])
+
+print()
+print(reversed(myweb), ' <<<<')
+print(list(reversed(myweb)), '<< list')
+print(str(reversed(myweb)), '<< string')
+print(tuple(reversed(myweb)), '<< tuple')
