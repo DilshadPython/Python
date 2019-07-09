678e1e61fef63a2b26404a3016731c4aadf136a5
Dilshad Abdulla <dilshad.a73@gmail.com>
2019-07-09 23:29:10 +0100

    How to read numbers as word, you need to install python-inflect package library.

---
+++ spell-num/spell_num.py
@@ -0,0 +1,15 @@
+# How to spelling number using inflect 
+
+# pip3 install inflect # (VN)
+# sudo apt-get install python-inflect  # in the terminal 
+
+import inflect
+
+num1 = 6736743
+num2 = 21451873
+
+e = inflect.engine()
+
+print(e.number_to_words(num1))
+print('\n ======== ')
+print(e.number_to_words(num2))
