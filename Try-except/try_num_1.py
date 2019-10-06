a7251ed5d995bfc50d3c3fc79bf0484decd5f5f9
Dilshad Abdulla <dilshad.a73@gmail.com>
2019-10-06 19:45:14 +0100

    Using try funtion howit work try and when try not working reach to except

---
+++ Try-except/try_num_1.py
@@ -0,0 +1,10 @@
+num = 'hi'
+
+try:
+    print('The is true because we try to convert it to integer')
+    istr = int(num)
+except:
+	print('This is working because -1 it\'s ')
+	istr = -1
+
+print('Check second', istr)
