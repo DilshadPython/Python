3b8e38ed71c6f560956b108f6882db037dfcc54b
Dilshad Abdulla <dilshad.a73@gmail.com>
2019-10-06 17:26:40 +0100

    If and elif are not runing because the condition not match and jump to else, using for loop in if condition how it works and back to the end of if.

---
+++ If-Statment/elif.py
@@ -0,0 +1,9 @@
+
+num = 16
+if num < 3:
+    print('Yes that is True')
+elif num > 20:
+    print(' No value greater')
+else:
+    print('The if and elif are not runing here because num is signed to 16.')
+print('\n close the if and elif statment')
---
+++ If-Statment/if_for.py
@@ -0,0 +1,15 @@
+a = 10
+if a > 5:
+    print('The number', a, ' is higher than a')
+    print('Continue heigher ')
+print('End of first if statment')
+
+print('\n')
+print('####' * 10)
+
+for x in range(10):
+    print(x)
+    if x > 5:
+        print('The number ', x, ' above the range middle')
+    print('The ', x, ' is less than middle')
+print('End of the program!')
