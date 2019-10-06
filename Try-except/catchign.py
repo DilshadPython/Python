b70d7d1e05b9dd22b762e4df29db23b564095574
Dilshad Abdulla <dilshad.a73@gmail.com>
2019-10-06 19:43:30 +0100

    Using Try to count from 1 to when the user press ctrl and c on the keyboard, after we will see how many number has been counted until ctrl+c entered

--- Try-except/catchign.py
+++ Try-except/catchign.py
@@ -1,4 +1,7 @@
-
+'''
+'Using Try to count from 1 to when the user press ctrl and c on the keyboard,
+ after we will see how many number has been counted until ctrl+c entered
+'''
 c = 0  # This is counter
 
 print("Press crtl and c")
