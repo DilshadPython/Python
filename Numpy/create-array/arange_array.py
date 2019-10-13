1e0f13d3319715867e351634c37fcc64cf27772b
Dilshad Abdulla <dilshad.a73@gmail.com>
2019-10-13 12:06:06 +0100

    Add some info for refresh

--- Numpy/create-array/arange_array.py
+++ Numpy/create-array/arange_array.py
@@ -9,5 +9,6 @@ print('=============================================')
 # start from 7
 # stoped at 77
 # steps by 3
+print('Start from 7 and add 3 and so on until reach to 77')
 text = np.arange(7, 77, 3)
 print(text)
