01a8c6ba4401cf8ebce70465370396e80d5c8b6c
DilshadGit <dilshad.abdulla@icloud.com>
2020-04-23 01:50:23 +0100

    We create a models named tools and the current file is linked tp the models fole named tools. We import the tools and call the method throw the models to the methods

---
+++ Module/link_tools.py
@@ -0,0 +1,9 @@
+import tools
+
+'''
+References: https://docs.python.org/3/py-modindex.html
+We create a models named tools and the current file is linked
+tp the models fole named tools. We import the tools and call the
+method throw the models to the methods.
+'''
+print(tools.roll_dice(int(input('Enter a number: '))))
