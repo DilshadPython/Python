e2e33099ad6a57cf946145d19d9acce5609b0bc6
DilshadGit <dilshad.abdulla@icloud.com>
2020-04-24 16:55:16 +0100

    How to use f string and format in python change the float number with1, 2,3 digit number after dot.

---
+++ Strings/f_string.py
@@ -0,0 +1,15 @@
+'''
+How to use f strings
+'''
+
+name = str(input('Enter a name: '))
+lang = str(input('Enter a language: '))
+
+print(f'Hello {name}, do you like to learn {lang} programming language?, ')
+
+print('#################################################################')
+
+new_name = ''
+text = 'Hello, ' + input("Enter a name: ")
+
+print(text + f"{new_name}")
