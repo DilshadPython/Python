68c4621393c5932324cc9d1407f836e39cee41a8
DilshadGit <dilshad.abdulla@icloud.com>
2020-07-07 13:22:21 +0100

    Display the type of n and a after walrus operator.

--- Python3.8/walrus_list.py
+++ Python3.8/walrus_list.py
@@ -1,5 +1,12 @@
 a = [1, 2, 3, 4, 6, 7, 8, 33, 6, 9, 98, 22, 12, 23]
 
+if len(a) >= 10:
+	print(f"List is too long ({a} elements, expected <= 10)")
+
+## Output before using walrus operator
+#>>>List is too long ([1, 2, 3, 4, 6, 7, 8, 33, 6, 9, 98, 22, 12, 23] elements, expected <= 10)
+
+
 if (n := len(a)) > 10:
 	print(f"List is too long ({n} elements, expected <= 10)")
 
@@ -11,3 +18,8 @@ b = [6, 7, 8, 33, 6, 9, 98, 22, 12]
 
 if (m := len(b)) < 10:
 	print(f"List is too short ({m} elements, expected >= 10)")
+
+# >>> print(type(a))
+# <class 'list'>
+# >>> print(type(n))
+# <class 'int'>
