d55adbf3a7cae9bf21b8728b21ef0ac4c226a4e1
DilshadGit <dilshad.abdulla@icloud.com>
2020-12-25 16:00:57 +0000

    This file has benn add need some nore info to understanding better.

---
+++ File/iter_math_file.py
@@ -0,0 +1,14 @@
+import itertools
+import math
+
+#create empty list
+data = []
+
+with open('math.txt') as mf:
+    # use list comperhenstion
+    data = [int(x) for x in mf.readlines()]
+for size in range(2, 4):
+    for current in itertools.combinations(data, size):
+        if sum(current) == 2020:
+            print(math.prod(current))
+            break
