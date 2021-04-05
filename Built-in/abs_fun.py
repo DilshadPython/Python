56a10a01617dfea67bfa4b24a42c0e65c38b657c
DilMac <dilshad.a73@gmail.com>
2021-04-05 23:13:17 +0100

    Show examples of abs function and how __class__ works.

---
+++ Built-in/abs_fun.py
@@ -0,0 +1,29 @@
+
+number = -8
+
+print('Absolute value of -8 is: ', abs(number))
+
+print('\n')
+fnum = -3.76
+print(f'Absolute value of {fnum} is: ', abs(fnum))
+
+print('\n')
+complex_num = 2 - 3j
+print(f'Absolute value of {complex_num} is: ', abs(complex_num))
+
+print("Python abs() function for complex numbers example\
+ When a complex number is passed as an argument to abs() function,\
+it returns the magnitude of the complex number.\
+The magnitude of a complex number a + bj is equal to √a2+b2.")
+
+print('\n')
+complex_num = 4 - 5j
+print(f'Absolute value of {complex_num} is: ', abs(complex_num))
+
+print('\n')
+complex_num = 3 - 3j
+print(f'Absolute value of {complex_num} is: ', abs(complex_num))
+
+print('\n')
+complex_num = 3 - 4j
+print(f'Absolute value of {complex_num} is: ', abs(complex_num))
--- Built-in/all_class.py
+++ Built-in/all_class.py
@@ -5,5 +5,5 @@ import re
 for x in dir(builtins):
     if re.match(r'^[a-z]', x):
         print(x)
-        print(getattr(builtins, x).__doc__)
+        print(getattr(builtins, x).__class__)
         print('*' * 79)
