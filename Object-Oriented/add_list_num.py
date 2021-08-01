04f7a0ecf6924665200a1b1211b268801b870d43
DilshadGit <dilshad.a73@gmail.com>
2021-08-01 22:21:02 +0100

    Update style.

--- Object-Oriented/add_list_num.py
+++ Object-Oriented/add_list_num.py
@@ -1,14 +1,14 @@
 class ListNumber(object):
-	def __init__(self, new_num):
-		self.main_list = new_num
+    def __init__(self, new_num):
+        self.main_list = new_num
 
-	def __add__(self, second_num):
-		mylst = [x + y for x, y in zip(self.main_list, second_num.main_list)]
+    def __add__(self, second_num):
+        mylst = [x + y for x, y in zip(self.main_list, second_num.main_list)]
 
-		return ListNumber(mylst)
+        return ListNumber(mylst)
 
-	def __repr__(self):
-		return str(self.main_list)
+    def __repr__(self):
+        return str(self.main_list)
 
 
 flst_1 = ListNumber([1, 2, 3, 4, 5, 6, 3, 4])
@@ -16,4 +16,3 @@ slst_2 = ListNumber([10, 100, 200, 3000, 500, 400, 800, 700])
 
 total = flst_1 + slst_2
 print(total)
-
