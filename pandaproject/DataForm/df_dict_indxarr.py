a1d9fda557c22588fdbbce8c8d3d2b71fa107fe1
DilshadGit <dilshad.a73@gmail.com>
2021-12-06 21:13:41 +0000

    Create new dataframe using dictionary with index array.

---
+++ pandaproject/DataForm/df_dict_indxarr.py
@@ -0,0 +1,11 @@
+# create dataframe using dictionary
+
+import pandas as pd
+
+# create data using dictionary
+
+data = {'Name': ['Audi', 'Mercedes', 'BMW', 'Volkswagen', 'Volvo'], 'Models': ['A5', 'XM', 'X5', 'Passat', 'V90']}
+
+df = pd.DataFrame(data, index=['Branch1', 'Branch2', 'Branch3', 'Branch4', 'Branch5'])
+
+print(df)
