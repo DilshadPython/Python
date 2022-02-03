85e42631940b9a7178e8dfcbf6497acf483112cd
DilshadGit <dilshad.a73@gmail.com>
2022-02-03 19:06:19 +0000

    Adding some example of for loops.

---
+++ For-loop/exculator.py
@@ -0,0 +1,8 @@
+floors = 12
+
+floor = input('Enter your floor number between 1 - 12 : ')
+
+for floor in floors:
+	num = int(floor) + 1
+	if num == floor:
+		print('You reached to your floor number, ', floor) 
