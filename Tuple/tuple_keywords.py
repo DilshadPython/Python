cda174cf03f5376df1a7a42a601f2981f75d7d87
DilshadGit <dilshad.a73@gmail.com>
2022-02-03 22:05:24 +0000

    Adding extra file to show that we can not add another name to tuple.

---
+++ Tuple/tuple_keywords.py
@@ -0,0 +1,13 @@
+tpl = ('Python', 'Java', 'PHP', 'JavaScript')
+
+create_tpl = tpl 
+
+print(tpl)
+print(create_tpl)
+
+print("\n You can't use index as adding extra name to the tuple because immutable!")
+
+"""
+tpl[0] = 'MySQL'
+print(tpl)
+"""
