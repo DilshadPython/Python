ed3a4b9760a8000b12d87d10ca6a80c2cb1bb501
DilshadGit <dilshad.a73@gmail.com>
2022-02-03 19:05:11 +0000

    Adding some example of if statments.

---
+++ If-Statment/if_2.py
@@ -0,0 +1,10 @@
+num = int(input('Enter a number: '))
+minimum_num = 40
+maximum_num = 65
+
+
+if num > minimum_num:
+	print(num, ' is greater than 40')
+	if num < maximum_num:
+		print(num, ' is smaller than 65')
+print('Program ended')
---
+++ If-Statment/if_3.py
@@ -0,0 +1,13 @@
+num = int(input('Enter a number: '))
+small = 5
+medium = 10
+large = 15
+
+
+if num < small:
+	print(num, ' the size is S = ', small)
+elif num < medium:
+	print(num, ' the size is M = ', medium)
+else:
+	print(num, ' the size is L ', large)
+
