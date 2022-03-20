c5975d4da48a47bd458389af5245b9e6cabee29e
DilshadGit <dilshad.a73@gmail.com>
2022-03-20 22:56:51 +0000

    Create method in the class and using self to access the method and arguments in this method

---
+++ OOP-EX/README.md
@@ -0,0 +1,10 @@
+# We create class Product
+
+# obj = Product() create an object of the class
+
+# create an method in the class and self has to be given as first parameter in the function
+# which is repersents the instance of this class, by using self keyword we can access the
+# attribute s and methods of this class in python.
+
+def calcuate_total_price(self)
+
--- OOP-EX/main.py
+++ OOP-EX/main.py
@@ -1,5 +1,6 @@
 class Product:
-    pass
+    def calculate_total_price(self, i, j):
+        return i * j
 
 
 # Create an object of the class
@@ -9,7 +10,9 @@ obj.item_name = str('Moble')
 obj.item_price = float(49.99)
 obj.quantity = int(234)
 
-total_price = obj.item_price * obj.quantity
+# total_price = obj.item_price * obj.quantity
+# The obj stand for self in the method, obj.item_price stand for i and obj.quantity stand for j
+obj.calculate_total_price(obj.item_price, obj.quantity)
 
 print(type(obj.item_name))
 print(type(obj.item_price))
