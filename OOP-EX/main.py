985966b940389457998c573e6a1e8b7a25e486b1
DilshadGit <dilshad.a73@gmail.com>
2022-03-20 23:47:43 +0000

    create constractor method __init__ and added the attribute to the constructor, next assignedall attribute in constructor method by using self and we can access to each attribut in second method using self whithout given any parameter in th calculate_total_price method.

--- OOP-EX/main.py
+++ OOP-EX/main.py
@@ -1,31 +1,51 @@
 class Product:
-    def calculate_total_price(self, i, j):
-        return i * j
+    # we initionalise the class by creating __init__ method or constractor
+    # we can also give the quantity to each product in init method directly and removed from created obj
+    def __init__(self, name, price, quantity=0):
+        print(f'We initialized the class by created an instance: {name}, {price}, {quantity}')
+        # we assigned the class to each attribute
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    # def calculate_total_price(self, i, j):
+    #     return i * j
+    # while we assiged the price and quantity in the class we can access to the attribute by using self
+    # and removed i, j from the method
+    def calculate_total_price(self):
+        return self.price * self.quantity
 
 
 # Create an object of the class
-obj = Product()
+# obj = Product()
+# now we add the name directly to the class
+# obj = Product('Mobile', 49.99, 44)
+obj = Product('Mobile', 49.99, 17)
 
-obj.item_name = str('Mobile')
-obj.item_price = float(49.99)
-obj.quantity = int(234)
+# obj.name = str('Mobile')
+# obj.price = float(49.99)
+# obj.quantity = int(234)
 
 
-# total_price = obj.item_price * obj.quantity
+# total_price = obj.price * obj.quantity
 # The obj stand for self in the method, obj.item_price stand for i and obj.quantity stand for j
-print(obj.item_name)
-print(obj.calculate_total_price(obj.item_price, obj.quantity))
+# print(obj.name)
+# print(obj.calculate_total_price(obj.price, obj.quantity))
+print(obj.calculate_total_price())
 
 # Here we create another obj from the same class and adding new items the attribute not changed but the new obj1
 # is given to display different result
-obj1 = Product()
-obj1.item_name = str('Television')
-obj1.item_price = float(159.48)
-obj1.quantity = int(74)
+# obj1 = Product()
+# obj1 = Product('Television', 159.48, 17)
+obj1 = Product('Television', 159.48, 29)
+# obj1.item_name = str('Television')
+# obj1.item_price = float(159.48)
+# obj1.quantity = int(74)
 
-print(obj1.item_name)
-print(obj1.calculate_total_price(obj1.item_price, obj1.quantity))
+# print(obj1.name)
+# print(obj1.calculate_total_price(obj1.price, obj1.quantity))
+print(obj1.calculate_total_price())
 
-print(type(obj.item_name))
-print(type(obj.item_price))
-print(type(obj.quantity))
+# print(type(obj.name))
+# print(type(obj.price))
+# print(type(obj.quantity))
