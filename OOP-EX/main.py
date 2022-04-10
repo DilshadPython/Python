a003e0a68ffde5ac73011609a0c3b171618ce8f0
DilshadGit <dilshad.a73@gmail.com>
2022-04-10 12:53:26 +0100

    Use __dict__ to shows the attribute for both class attribut and instance attribute saperate pay_discount belong to class attribut from instance attribute. next we created get_discount() method to showing how the class attribute works when we use Product.pay_discount and self.pay_discount and explain the different when the pay_discount updated to different numbers.

--- OOP-EX/main.py
+++ OOP-EX/main.py
@@ -1,6 +1,6 @@
 class Product:
-    # we create attribute for the class
-    discount = 0.7
+    # we create attribute for the class attribute, this is if the payment more then 25% discount
+    pay_discount = 0.73
 
     # we initionalise the class by creating __init__ method or constractor
     # we can also give the quantity to each product in init method directly and removed from created obj
@@ -24,27 +24,28 @@ class Product:
     def calculate_total_price(self):
         return self.price * self.quantity
 
+    # create method to access to pay_discount
+    def get_discount(self):
+        '''
+        if you use Product instead of self when you update pay_discount doesn't change the
+        discount for instance item, but if you changed to self the discount will be update in line
+        where we update pay_discount: obj1.pay_discount = 0.623
+        '''
+        # self.price = self.price * Product.pay_discount
+        self.price = self.price * self.pay_discount
+
 
 # Create an object of the class
 # obj = Product()
 # now we add the name directly to the class
 # obj = Product('Mobile', 49.99, 44)
 obj = Product('Mobile', 49.99, 17)
-
-print(Product.discount)
-
 # obj.name = str('Mobile')
 # obj.price = float(49.99)
 # obj.quantity = int(234)
 
-
-# total_price = obj.price * obj.quantity
-# The obj stand for self in the method, obj.item_price stand for i and obj.quantity stand for j
-# print(obj.name)
-# print(obj.calculate_total_price(obj.price, obj.quantity))
-print(obj.calculate_total_price())
-
-# Here we create another obj from the same class and adding new items the attribute not changed but the new obj1
+# Here we create another obj from the same class and adding new items the attribute not changed but the
+# new obj1
 # is given to display different result
 # obj1 = Product()
 # obj1 = Product('Television', 159.48, 17)
@@ -52,6 +53,13 @@ obj1 = Product('Television', 159.48, 29)
 # obj1.item_name = str('Television')
 # obj1.item_price = float(159.48)
 # obj1.quantity = int(74)
+
+
+# total_price = obj.price * obj.quantity
+# The obj stand for self in the method, obj.item_price stand for i and obj.quantity stand for j
+# print(obj.name)
+# print(obj.calculate_total_price(obj.price, obj.quantity))
+print(obj.calculate_total_price())
 
 # print(obj1.name)
 # print(obj1.calculate_total_price(obj1.price, obj1.quantity))
@@ -61,10 +69,28 @@ print(obj1.calculate_total_price())
 # print(type(obj.price))
 # print(type(obj.quantity))
 
-# This will display all attributes for class level
-print(Product.__dict__)
+# This is the class attribute
+print('This is the class attribute: ', Product.pay_discount)
+# This is instance attribute, because the pay_discount has not been define in instance level (__init__)
+print('This instance level: ', obj.pay_discount)
+print('This instance level: ', obj1.pay_discount)
 
-# This will display all attributes for instance level
-print(obj.__dict__)
+print()
+# This will display all attributes belong to class level
+print('This will display all attributes belong to class level: ', Product.__dict__, '\n')
+'''
+Infor the __dict__ will saperate which attribute belong to class level and which which
+attribute belong to instance level, inother words you can see the pay_discount in class attribute but
+you don't see the pay_discount in instance attribute, you can use as debug I mean __dict__
+'''
+# This will display all attributes belong to instance level
+print('This will display all attributes belong to instance level: ', obj.__dict__, '\n')
 
-# stopped in 36 minute
+# Here calculte the get_discount
+obj.get_discount()
+print('Here we get discount for the price it will be 0.73 : ', obj.price)
+
+obj1 = Product('Mobile', 49.99, 4)
+obj1.pay_discount = 0.623
+obj1.get_discount()
+print('After update the pay desicount it will be 0.623 : ', obj1.price)
