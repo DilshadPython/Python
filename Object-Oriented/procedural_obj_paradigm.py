d3fe4efc56faaececf20e9ce1ada54716526278b
DilshadGit <dilshad.a73@gmail.com>
2022-12-13 19:37:53 +0000

    Explain Procedural Paradigm and Object Paradigm
    This is example of Procedural program and OO program

---
+++ Object-Oriented/procedural_obj_paradigm.py
@@ -0,0 +1,23 @@
+
+# This is procedural paradigm
+var = 2
+var += 1
+var += 1
+var += 1
+
+
+print('\n This is procedural Paragigm')
+print(var)
+
+# Object Paradigm
+class CustomerNo():
+	pass
+
+obj = CustomerNo()
+obj.num1 = 1
+obj.num1 += 1
+
+
+
+print('\n This is how object created!')
+print(obj.num1)
