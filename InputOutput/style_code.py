d908211d8b140c5a8126b63cf98815b35795eca1
DilshadGit <dilshad.a73@gmail.com>
2023-01-12 15:21:21 +0000

    Show how to use print on the screen using %

---
+++ InputOutput/style_code.py
@@ -0,0 +1,13 @@
+# one way print word
+my_city = 'Sulaimany'
+
+print(my_city)
+
+# format of display we use %
+my_name = 'Dilshad'
+blood_pressure = 75.875201
+
+# to display the both var use format % 
+# .2f will keep 2 number after . remove the other [5201]
+result = 'Name = %s : Blood pressure = %.2f ' % (my_name, blood_pressure)
+print(result)
