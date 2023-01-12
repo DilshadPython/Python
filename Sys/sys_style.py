909577089253e4fc16615e1ff61108c50acdb290
DilshadGit <dilshad.a73@gmail.com>
2023-01-12 16:43:54 +0000

    Import sys and how to use stdout in python scripts explain the different between sys.stdout.write and print .

---
+++ Sys/sys_style.py
@@ -0,0 +1,12 @@
+# we import sys from python library
+import sys
+
+# sys.stdout.write is almost like print because we will print
+# Welcome to Python3 to the screen
+sys.stdout.write('Welcome to Python3\n')
+
+print()
+useprint = sys.stdout.write('We use print here see if it working.')
+print(useprint)
+
+print()
