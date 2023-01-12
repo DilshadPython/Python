16456ac0beef7f75d345dd46abdbc7672b5bcea1
DilshadGit <dilshad.a73@gmail.com>
2023-01-12 23:41:53 +0000

    How ro use input

---
+++ Sys/input_python.py
@@ -0,0 +1,11 @@
+# how to use input and % format
+
+number = int(input('Enter a number: '))
+
+print('\nEnter your second number \n')
+
+numr_1 = int(input('Next number: '))
+
+result = 'The sume is ' + str(number + numr_1)
+
+print('Result : ', result)
