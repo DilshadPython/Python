09fb46561be0acefb1d6a901090aa6d3dda92a82
DilshadGit <dilshad.a73@gmail.com>
2023-01-14 21:44:51 +0000

    Explain how for loop working with list tuple and dictionary with some different way and options.

--- For-loop/for_dic_key.py
+++ For-loop/for_dic_key.py
@@ -19,6 +19,6 @@ for age in names.values():
 	print(age, end=' ')
 
 print('\n=========================================\n')
-print('for name in names.values()')
+print('for name in names.keys()')
 for key in names.keys():
-	print(key, ' and his age is', names[key], end=' ')
+	print(key, ' and his age is', names[key])
