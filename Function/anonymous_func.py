3cbbcc8ad1258d1c50afe576b468dd4ac18b8b1f
DilshadGit <dilshad.a73@gmail.com>
2023-01-18 02:08:28 +0000

    create anonymous function using lambda. using list map and lambda.

--- Function/anonymous_func.py
+++ Function/anonymous_func.py
@@ -11,3 +11,10 @@ This example above is exactly the same like lambda
 sq = lambda x: x*x
 
 print(sq(int(input('Enter any number: '))))
+
+print('\n Another anonymous function using list, map lambda:')
+# create another anonymous function using map with lambda
+numbers = range(1, 9)
+sqroot = list(map(lambda x: x*x, numbers))
+
+print(sqroot)
