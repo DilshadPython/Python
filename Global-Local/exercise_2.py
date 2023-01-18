1524e3b80e1f82a1270a2537626ee5d7e34efcb2
DilshadGit <dilshad.a73@gmail.com>
2023-01-18 17:26:54 +0000

    Combine multiple methods in one method and call one after another.

---
+++ Global-Local/exercise_2.py
@@ -0,0 +1,31 @@
+# this is nested function and lecsecal scoping
+
+
+import math 
+
+'''
+we are conbine the perivouse exercise all function in one function as nested scope function
+fabs() ==>> absolute values from number
+'''
+def average(x, y):
+	return (x + y) / 2
+
+def square(number):
+	return number * number
+
+# this is the main function
+def sqrt(number):
+	def closeEnough(guess):
+		return (math.fabs((square(guess)) - number) < 0.001)
+	def improve(guess):
+		return average(guess, (number / guess))
+	def sqrtHelper(guess):
+		if (closeEnough(guess)):
+			return guess
+		else:
+			return sqrtHelper(improve(guess))
+	return sqrtHelper(1.0)
+
+
+num = int(input('Enter a number: '))
+print(sqrt(num))
