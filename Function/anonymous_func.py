38fdc2c2b6844ba96d136d38548063c9a7b2e5bb
DilshadGit <dilshad.a73@gmail.com>
2023-01-18 02:02:10 +0000

    create anonymous function using lambda.

---
+++ Function/anonymous_func.py
@@ -0,0 +1,13 @@
+# Anonymous function is a function doesn't have a name example lambda
+# structure of lambda function
+# lambda x: x*x 
+
+'''
+def sequre(num):
+	return num * num
+This example above is exactly the same like lambda
+'''
+
+sq = lambda x: x*x
+
+print(sq(int(input('Enter any number: '))))
