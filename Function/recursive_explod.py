c2e62b9b4dbe358ac4f1d6c3642914f875e31835
DilshadGit <dilshad.a73@gmail.com>
2023-01-19 01:47:41 +0000

    update.

--- Function/recursive_explod.py
+++ Function/recursive_explod.py
@@ -1,7 +1,7 @@
 # Example of explod ==> explode('Python')-> 'P y t h o n'
 
 def recursive_explode(word):
-	if len(word) <=1 :
+	if len(word) <= 1:
 		return word
 	else:
 		return word[0] + ' ' + recursive_explode(word[1:])
