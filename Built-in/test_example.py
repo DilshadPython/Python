0cfaec9a1edf70a1e5b901846c49ccb20d2da329
DilshadGit <dilshad.a73@gmail.com>
2023-01-19 02:20:36 +0000

    Create some math modules and called from other file as built-in modules.

---
+++ Built-in/test_example.py
@@ -0,0 +1,7 @@
+from example import *
+
+rad = int(input('Enter the radius to find the area: '))
+print('The area is :', area(rad))
+
+vol = float(input('Enter raduis to find a volume: '))
+print('The volume is: ', volume(vol))
