44d73d0bdb8efdb424729c66784102ad1f47c15f
DilshadGit <dilshad.a73@gmail.com>
2023-01-23 23:53:42 +0000

    Explain encapsulation rule how setter and getter working together

---
+++ Object-Oriented/Encapsulation/set_and_get.py
@@ -0,0 +1,29 @@
+
+# This is example of Encapsulation class
+
+class House:
+	def set_price(self, price):
+		print('\nSETTER METHODS: set_price is set the house price')
+		self.price = price
+
+	def get_price(self):
+		print('\nGETTER METHODS: get_price is get the price of the house has been set or given.')
+		return self.price
+
+obj1 = House() 
+obj2 = House() 
+
+obj1.set_price(745662)
+obj2.set_price(850434)
+print('\nWe can use set the price here as well not in the set_price of SETTER METHODS!')
+obj1.price = '444444'
+obj2.price = 123123
+print(obj2.price)
+
+
+
+print(obj1.get_price())
+print(obj2.get_price())
+
+print('First house cost: ', obj1.get_price(), '£')
+print('Second house cost: ', obj2.get_price(), '£')
---
+++ Object-Oriented/Encapsulation/the_integer.py
@@ -0,0 +1,47 @@
+
+# Encapsulation class
+
+class TheInteger(object):
+	def sent_number(self, num):
+		try:
+			num = int(num)
+		except ValueError:
+			return
+		self.num = num
+
+
+	def get_number(self):
+		return self.num
+
+
+	def increment_num(self):
+		self.num = self.num + 1
+
+obj = TheInteger()
+
+# we set only int
+obj.sent_number(19)
+print(obj.get_number())
+
+# try another value but not int
+obj.sent_number('Welcome')
+print(obj.get_number())
+
+# try another value but float this time
+obj.sent_number(88.06)
+print(obj.get_number(), '<< Return int not float!')
+
+# We try another instance obj
+new_obj = TheInteger()
+new_obj.num = 22
+print(new_obj.increment_num())
+
+# we try string here we get an error can't concatenate str to int
+# new_obj.num = 'Python'
+# print(new_obj.increment_num())
+
+try:
+	new_obj.num = 'Python3'
+	print(new_obj.increment_num())
+except:
+	print('Error the number is str not int : ', new_obj.num)
