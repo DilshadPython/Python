891e937185427919b59647ec97884e4e3cd1fd1f
DilshadGit <dilshad.a73@gmail.com>
2023-01-24 02:33:18 +0000

    change inheritance to Inheritance.

---
+++ Object-Oriented/Inheritance/README.md
@@ -0,0 +1,6 @@
+Inheritance Hierachy
+
+1. Classes can be orginzed into an inheritance hierachy
+2. A child class can access the attribute of all parent (grandparent, etc)classes
+3. Inheritance promotes code collaboration and reuse
+4. Code not need to be appear twice.
---
+++ Object-Oriented/Inheritance/parents.py
@@ -0,0 +1,52 @@
+''' 
+Inheritance allowed us to inheritace a attribute and method in the parent class 
+'''
+
+# clas variables
+
+
+class Parent:
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def parent_detail(self):
+        return '{}, {}, {}, {}'.format(self.name, self.last_name, self.age)
+
+    def fullname(self, first_name, last_name):
+        return '{} {}'.format(self.first_name, self.last_name)
+
+    def email_address(self):
+        return '{}.{}'.format(self.first_name, self.last_name) + '@' + 'gmail.com'
+
+obj = Parent('Tim', 'Alan', '44')
+print(obj.first_name + ' ' + obj.last_name + ' ' + obj.age)
+
+
+class Son(Parent):
+    pass
+
+
+class Daughter(Parent):
+    pass
+
+
+obj_1 = Daughter('Adam', 'Sophia', '25')
+obj_2 = Daughter('Adam', 'Claudia', '31')
+obj_3 = Son('David', 'Tom', '33')
+obj_4 = Son('David', 'Jeff', '29')
+
+
+print(obj_1.first_name + ' ' + obj_1.last_name + ' ' + obj_1.age)
+print(obj_1.email_address())
+print(obj_2.first_name + ' ' + obj_2.last_name + ' ' + obj_2.age)
+print(obj_2.email_address())
+
+print('=============================================================')
+
+print(obj_3.first_name + ' ' + obj_3.last_name + ' ' + obj_3.age)
+print(obj_3.email_address())
+print(obj_4.first_name + ' ' + obj_4.last_name + ' ' + obj_4.age)
+print(obj_4.email_address())
