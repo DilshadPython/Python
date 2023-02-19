e15012ea612ef5cfdee3ba6d6b2c230907d6d9cb
DilshadGit <dilshad.a73@gmail.com>
2023-02-19 23:18:40 +0000

    Using pickle to store data from the script to the file using different python utilities such as list int, str, tuple, dict and class. Unpickled to read the date from the file.

--- Serialization/store_dict_data.py
+++ Serialization/store_dict_data.py
@@ -17,9 +17,11 @@ print('To store data into the file enter 1 to read the data from file enter 2 :'
 var = int(input('Enter 1 or 2: \n'))
 
 if var == 1:
+	# we store the multi different data to the file
     with open('dictdata.txt', 'wb') as tmd:
         pickle.dump((number, message, mydict_list, query_output), tmd)
 elif var == 2:
+	# read the data from the file
     with open('dictdata.txt', 'rb') as tmd:
         tupledata = pickle.load(tmd)
         print(tupledata[0])
