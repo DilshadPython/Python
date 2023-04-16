e74e7a8f550112f90523d81dcf2d5193ef7d02e6
DilshadGit <dilshad.a73@gmail.com>
2023-04-16 04:27:19 +0100

    Read indexing as string

---
+++ Index/string.py
@@ -0,0 +1,16 @@
+leason = 'Welcome to Python tutorials'
+
+contents = '''
+Django is a popular Python-based web framework. It is batteries included which 
+dramatically speeds up web development but also comes with a bit of a learning
+curve for newcomers. Whether you are new to web development or already experienced, 
+here is an up-to-date list of the best Django books.
+'''
+
+print(leason[0])
+print(leason[0:])
+print(leason[0:11])
+print(leason[-20:-1])
+print(leason[:-1])
+
+print(contents)
