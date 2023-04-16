76ee6b8b23434762b8c3f354a6e56a6e194be852
DilshadGit <dilshad.a73@gmail.com>
2023-04-16 04:44:53 +0100

    Read indexing as string

--- Index/string.py
+++ Index/string.py
@@ -7,10 +7,23 @@ curve for newcomers. Whether you are new to web development or already experienc
 here is an up-to-date list of the best Django books.
 '''
 
-print(leason[0])
-print(leason[0:])
-print(leason[0:11])
-print(leason[-20:-1])
-print(leason[:-1])
+print('\n line 10: >> ', leason[0])  # first char
+print('\n line 11: >> ', leason[0:])  # first to the end
+# first char to char 10 the 11 not include
+print('\n line 12: >> ', leason[0:11])
+print('\n line 13: >> ', leason[:9])  # form first to char 8
+print('\n line 14: >> ', leason[-20:-1])  # reverse from last char to 19 char
+print('\n line 15: >> ', leason[:-1])  # start from last to begginning
 
-print(contents)
+print('\n line 17: >> ', contents)  # print all
+print('\n line 18: >> ', contents[21:60])  #
+
+test = 'It is batteries included which dramatically speeds up web development but also comes with '
+
+copy_test = test[:]
+
+print('\n line 24: >>', copy_test)
+
+name = 'Dilshad Abdulla'
+
+print('\n line 29: >> ', name[1:-1])
