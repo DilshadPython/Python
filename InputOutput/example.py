dbe4d4c699c1fb22f669189089411935a15d9cbf
DilshadGit <dilshad.a73@gmail.com>
2023-04-16 04:45:48 +0100

    Change pound to kilogram

---
+++ InputOutput/example.py
@@ -0,0 +1,5 @@
+value = int(input('Enter the weight of pound: '))
+one_pound = 0.453592
+convert_to_kg = value * one_pound
+
+print('The weight in pound you entered is ', convert_to_kg, ' Kilograms')
