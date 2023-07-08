e4e7ef26c40001c0d0d8b17b58c780bcf10d999a
DilshadGit <dilshad.a73@gmail.com>
2023-07-08 18:19:09 +0100

    update.

--- Print/name.py
+++
@@ -1,7 +0,0 @@
-# strip() remove the whitespace and title() capitalize the 
-# first letter of firstname and lastname
-# Notice strip() remove the white space from beginning and 
-# the end of the name not in between
-fullname = input('Enter you full name:  ').strip().title()
-
-print(f'Hello, {fullname}')
