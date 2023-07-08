c5c7fdb89e7c2f5e1706c43301ef572285ee5bec
DilshadGit <dilshad.a73@gmail.com>
2023-07-08 18:17:54 +0100

    Create calculator and deciamel number .

--- 2.Numbers/calculator.py
+++ 2.Numbers/calculator.py
@@ -1,21 +1,23 @@
 print('Small calculation op means *, +, -, /, ^, //')
-def cal(num1=int(input("Enter num1: ")), op=input('Enter op: '), num2=int(input("Enter num2: "))):
-	if op == '+':
-		result = num1 + num2
-	elif op == '-':
-		result = num1 - num2
-	elif op == '*':
-		result = num1 * num2
-	elif op == '/':
-		result = num1 / num2
-	elif op == '^':
-		result = num1 ** num2
-	elif op == '%':
-		result = num1 % num2
-	elif op == '//':
-		result = num1 // num2
-
-	return print(result)
 
 
-cal()
+def call(num1=int(input("Enter num1: ")), op=input('Enter op: '), num2=int(input("Enter num2: "))):
+    if op == '+':
+        result = num1 + num2
+    elif op == '-':
+        result = num1 - num2
+    elif op == '*':
+        result = num1 * num2
+    elif op == '/':
+        result = num1 / num2
+    elif op == '^':
+        result = num1 ** num2
+    elif op == '%':
+        result = num1 % num2
+    elif op == '//':
+        result = num1 // num2
+
+    return print(result)
+
+
+call()
