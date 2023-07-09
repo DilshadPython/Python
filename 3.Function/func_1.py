07593a33fb9e93b9babe91c90c31217ff9d12b78
DilshadGit <dilshad.a73@gmail.com>
2023-07-09 16:37:15 +0100

    Update function 1.

--- 3.Function/func_1.py
+++ 3.Function/func_1.py
@@ -2,6 +2,7 @@ print('Enter your gender, m, f or None')
 
 sex = str(input('Enter your gender: '))
 
+
 def get_gender(sex=None):
     if sex == 'm':
         sex = 'Male'
@@ -13,5 +14,6 @@ def get_gender(sex=None):
         sex = 'Not ins the list'
 
     print(sex)
+
 
 get_gender(sex)
