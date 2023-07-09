63fe09e4963d099c1d5c57860613754f02cc77a6
DilshadGit <dilshad.a73@gmail.com>
2023-07-09 16:29:58 +0100

    Update function 1.

--- 3.Function/func_1.py
+++ 3.Function/func_1.py
@@ -1,18 +1,17 @@
+print('Enter your gender, m, f or None')
+
+sex = str(input('Enter your gender: '))
 
 def get_gender(sex=None):
-    if sex is 'm':
+    if sex == 'm':
         sex = 'Male'
-    elif sex is 'f':
+    elif sex == 'f':
         sex = 'Female'
-    elif sex is None:
+    elif sex == None:
         sex = 'None'
     else:
         sex = 'Not ins the list'
 
     print(sex)
 
-print('Call the function with four different char')
-get_gender('m')
-get_gender('f')
-get_gender()
-get_gender('H')
+get_gender(sex)
