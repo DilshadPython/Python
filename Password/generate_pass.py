8469259a613817ab6ab05978e40b03179e42de23
DilshadGit <dilshad.a73@gmail.com>
2023-12-06 15:42:19 +0000

    Adding the password to text file.

--- Password/generate_pass.py
+++ Password/generate_pass.py
@@ -1,14 +1,26 @@
 import random
 import string
+import datetime
 
 lower_char = 'abcdefghijklmnopqrstuvwxyz'
 upper_char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 num = '0123456789'
 symbole = '!"£$%^&*()-_=+~#@?\\<>/[]}{'
-all_together = lower_char + upper_char + num + symbole
+all_together = lower_char + upper_char + num #+ symbole
 
-length = 16
+length = 32
 
 password = ''.join(random.sample(all_together, length))
 
 print('Enter new password: ', password)
+f = open("example.txt", "a")
+x = datetime.datetime.now()
+f.write('\n')
+f.write(str(x))
+f.write(': ')
+f.write(password)
+
+f.close()
+
+# You can contact on this number +34 91 514 0000
+# Customerqueries@ryanair.com
