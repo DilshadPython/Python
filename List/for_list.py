b8eb3ef4923ae28b0cb5a7ab0e3e12b56afe515f
DilshadGit <dilshad.a73@gmail.com>
2023-12-06 15:32:38 +0000

    Create a list contain one randon number and 8 random numbers some of them duplicated but using set to remove but the issues is become 7 or 6 digit numbers have to find solution to always display 8 random numbers without duplication.

--- List/for_list.py
+++ List/for_list.py
@@ -7,8 +7,6 @@ my_id_num = [
 print(random.choices(my_id_num))
 
 
-numbers = [random.randint(1, 16) for _ in set(range(8))]
-non_duplicate = [set(numbers) for number in range(1)]
-print(non_duplicate)
+numbers = [random.randint(1, 16) for _ in range(10)]
 
-print(numbers, ' > Numbers')
+print(set(numbers)) # remove duplication
