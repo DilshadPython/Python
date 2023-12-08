64611b4dca3486e38ad2d164d7d43f069d70956d
DilshadGit <dilshad.a73@gmail.com>
2023-12-08 16:36:16 +0000

    Explain more float with e using in the number like 33e5 how it read with float

--- 2.Numbers/floats.py
+++ 2.Numbers/floats.py
@@ -8,3 +8,26 @@ print('Divid ', num1 / num2)
 print('Division with no reminder ', num1 // num2)
 print('Power ', num1 ** num2)
 print('Moduls reminder', num1 % num2)
+
+print('\n===========================')
+
+a = 3e8 # this bring 8 time 0 to front of 3 and change to float
+b = float(9)
+c = float('2548')
+d = float('nan')
+e = float('inf')
+f = float('-inf')
+
+x = 5e3
+y = 12e9
+z = 348e4
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
+print(f)
+print(x)
+print(y)
+print(z)
