ce7889e3f6b4b026b8d2abd8097ca998e23a1b36
DilshadGit <dilshad.a73@gmail.com>
2023-12-08 17:29:09 +0000

    How to convert number to complex.

--- 2.Numbers/bin_hex_oct_num.py
+++ 2.Numbers/bin_hex_oct_num.py
@@ -4,3 +4,7 @@ for x in range(numbers):
 	print('Binary of ', x, ' = ', bin(x), \
 		 'Hexadecamal of ', x, ' = ', hex(x), \
 		 'Octal of ', x, ' = ', oct(x))
+
+# Change number to complex
+for y in range(20):
+	print('The is ', y, 'Changed to complex is ', complex(y))
---
+++ 2.Numbers/example.py
@@ -0,0 +1,7 @@
+x = int('10000', 3)
+y = int('10000', 2)
+z = int('10000', 4)
+
+print(x)
+print(y)
+print(z)
