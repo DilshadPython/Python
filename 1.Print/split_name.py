f2c18613dab6cdd20f30caa927b0e85868365321
Dilshad Abdulla <dilshad.a73@gmail.com>
2024-05-12 21:29:22 +0100

    Expalin print how it works.

--- 1.Print/split_name.py
+++ 1.Print/split_name.py
@@ -1,4 +1,4 @@
-# print('objects', sep=' ', end='\n')')
+# print('objects', sep=' ', end='\n'', file=sys.stdout, flash=False)
 name = input('Please enter your full name. ').strip().title()
 
 fname, lname = name
