fd8d4015f511be97156e88839ad60b0d1e6cf953
Dilshad Abdulla <dilshad.a73@gmail.com>
2024-06-23 22:00:13 +0100

    How to delete date from the class using delattr.

--- Methods/delattr_def.py
+++ Methods/delattr_def.py
@@ -8,9 +8,17 @@ obj = Items()
 #Before
 print('The class data: ', obj.name, ' | ', obj.number, ' | ', obj.made)
 
+# check before delete
+item_number = hasattr(Items, 'number')
+print(item_number)
+
 #After delete all class data contents
 delattr(Items, 'number')
 
 # The Person object will no longer contain an "age" property
 
 print(obj.name, ' | ', obj.made)
+
+# Check after deleted
+item_number_after = hasattr(Items, 'number')
+print(item_number_after)
