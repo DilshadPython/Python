1f900167b498caad643ce80e334957671c3a3b4b
DilshadGit <dilshad.a73@gmail.com>
2024-11-16 14:48:16 +0000

    Create some methods and check if the codes are running well without conflict using unittest.

--- unittest/test_profiles.py
+++ unittest/test_profiles.py
@@ -5,50 +5,49 @@ from profiles import Student
 
 class TestStudent(unittest.TestCase):
 
-	def test_mail(self):
-		std_a = Student('Tom', 'David', 12370)
-		std_b = Student('Claudia', 'Ericson', 14760)
+    def test_mail(self):
+        std_a = Student('Tom', 'David', 12370)
+        std_b = Student('Claudia', 'Ericson', 14760)
 
-		self.assertEqual(std_a.mail, 'Tom.David@gmail.net')
-		self.assertEqual(std_b.mail, 'Claudia.Ericson@gmail.net')
-		
-		# adding new gmail account
-		std_a.firstname = 'Elena'
-		std_a.lastname = 'Alan'
-		std_b.firstname = 'Anton'
-		std_b.lastname = 'George'
+        self.assertEqual(std_a.mail, 'Tom.David@gmail.net')
+        self.assertEqual(std_b.mail, 'Claudia.Ericson@gmail.net')
 
-		self.assertEqual(std_a.mail, 'Elena.Alan@gmail.net')
-		self.assertEqual(std_b.mail, 'Anton.George@gmail.net')
+        # adding new gmail account
+        std_a.firstname = 'Elena'
+        std_a.lastname = 'Alan'
+        std_b.firstname = 'Anton'
+        std_b.lastname = 'George'
 
+        self.assertEqual(std_a.mail, 'Elena.Alan@gmail.net')
+        self.assertEqual(std_b.mail, 'Anton.George@gmail.net')
 
-	def test_student_profile(self):
-		std_a = Student('Tom', 'David', 12370)
-		std_b = Student('Claudia', 'Ericson', 14760)
+    def test_student_profile(self):
+        std_a = Student('Tom', 'David', 12370)
+        std_b = Student('Claudia', 'Ericson', 14760)
 
-		self.assertEqual(std_a.student_profile, 'Tom David')
-		self.assertEqual(std_b.student_profile, 'Claudia Ericson')
-		
-		# adding new student profile  
-		std_a.firstname = 'Elena'
-		std_a.lastname = 'Alan'
-		std_b.firstname = 'Anton'
-		std_b.lastname = 'George'
+        self.assertEqual(std_a.student_profile, 'Tom David')
+        self.assertEqual(std_b.student_profile, 'Claudia Ericson')
 
-		self.assertEqual(std_a.student_profile, 'Elena Alan')
-		self.assertEqual(std_b.student_profile, 'Anton George')
+        # adding new student profile
+        std_a.firstname = 'Elena'
+        std_a.lastname = 'Alan'
+        std_b.firstname = 'Anton'
+        std_b.lastname = 'George'
 
-	def test_apply_loan(self):
-		std_a = Student('Tom', 'David', 12370)
-		std_b = Student('Claudia', 'Ericson', 14760)
+        self.assertEqual(std_a.student_profile, 'Elena Alan')
+        self.assertEqual(std_b.student_profile, 'Anton George')
 
-		std_a.apply_loan()
-		std_b.apply_loan()
+    def test_apply_loan(self):
+        std_a = Student('Tom', 'David', 12370)
+        std_b = Student('Claudia', 'Ericson', 14760)
 
-								# to_pay, 
-		self.assertEqual(std_a.to_pay, 11504)
-		self.assertEqual(std_b.to_pay, 13726)
+        std_a.apply_loan()
+        std_b.apply_loan()
+
+        # to_pay,
+        self.assertEqual(std_a.to_pay, 11504)
+        self.assertEqual(std_b.to_pay, 13726)
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
--- unittest/test_university.py
+++ unittest/test_university.py
@@ -4,62 +4,61 @@ from university import Student
 
 
 class TestStudent(unittest.TestCase):
-	# This option use for very large testing and reduce repeat function
+    # This option use for very large testing and reduce repeat function
 
-	@classmethod
-	def setUpClass(cls):
-		print('setUpClass')
+    @classmethod
+    def setUpClass(cls):
+        print('setUpClass')
 
-	@classmethod
-	def tearDownClass(cls):
-		print('tearDownClass')
+    @classmethod
+    def tearDownClass(cls):
+        print('tearDownClass')
 
-	# setup method before the test
-	def setup(self):
-		print('setUp')
-		self.std_1 = Student('Tom', 'David', 12370)
-		self.std_2 = Student('Claudia', 'Ericson', 14760)
-	
-	# tearDown method run after the test
-	def tearDown(self):
-		print('tearDown\n')
+    # setup method before the test
+    def setup(self):
+        print('setUp')
+        self.std_1 = Student('Tom', 'David', 12370)
+        self.std_2 = Student('Claudia', 'Ericson', 14760)
 
-	# End of large test
+    # tearDown method run after the test
+    def tearDown(self):
+        print('tearDown\n')
 
-	def test_mail(self):
-		print('test_mail')
-		self.assertEqual(self.std_1.mail, 'Tom.David@gmail.net')
-		self.assertEqual(self.std_2.mail, 'Claudia.Ericson@gmail.net')
-		
-		# adding new gmail account
-		self.std_1.firstname = 'Elena'
-		self.std_2.firstname = 'Anton'
+    # End of large test
 
-		self.assertEqual(self.std_1.mail, 'Elena.Daivd@gmail.net')
-		self.assertEqual(self.std_2.mail, 'Anton.Ericson@gmail.net')
+    def test_mail(self):
+        print('test_mail')
+        self.assertEqual(self.std_1.mail, 'Tom.David@gmail.net')
+        self.assertEqual(self.std_2.mail, 'Claudia.Ericson@gmail.net')
 
+        # adding new gmail account
+        self.std_1.firstname = 'Elena'
+        self.std_2.firstname = 'Anton'
 
-	def test_profile(self):
-		print('test_profile')
-		self.assertEqual(self.std_1.profile, 'Tom David')
-		self.assertEqual(self.std_2.profile, 'Claudia Ericson')
-		
-		# adding new student profile  
-		self.std_1.firstname = 'Elena'
-		self.std_2.firstname = 'Anton'
+        self.assertEqual(self.std_1.mail, 'Elena.Daivd@gmail.net')
+        self.assertEqual(self.std_2.mail, 'Anton.Ericson@gmail.net')
 
-		self.assertEqual(self.std_1.profile, 'Elena David')
-		self.assertEqual(self.std_2.profile, 'Anton Ericson')
+    def test_profile(self):
+        print('test_profile')
+        self.assertEqual(self.std_1.profile, 'Tom David')
+        self.assertEqual(self.std_2.profile, 'Claudia Ericson')
 
-	def test_apply_loan(self):
-		print('test_apply_loan')
-		self.std_1.apply_loan()
-		self.std_2.apply_loan()
+        # adding new student profile
+        self.std_1.firstname = 'Elena'
+        self.std_2.firstname = 'Anton'
 
-								# to_pay, 
-		self.assertEqual(self.std_1.to_pay, 13359)
-		self.assertEqual(self.std_2.to_pay, 15940)
+        self.assertEqual(self.std_1.profile, 'Elena David')
+        self.assertEqual(self.std_2.profile, 'Anton Ericson')
+
+    def test_apply_loan(self):
+        print('test_apply_loan')
+        self.std_1.apply_loan()
+        self.std_2.apply_loan()
+
+        # to_pay,
+        self.assertEqual(self.std_1.to_pay, 13359)
+        self.assertEqual(self.std_2.to_pay, 15940)
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
