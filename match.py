53dc9630c9e39669943816a55b93ecbe023063cd
DilshadGit <dilshad.a73@gmail.com>
2025-01-08 02:05:16 +0000

    Explain how match function work in python. is almost like switch.

---
+++ match.py
@@ -0,0 +1,21 @@
+keyword = input("Enter the keywords? ")
+
+match keyword:
+    case 'print':
+        print('is Python')
+    case 'elif':
+        print('is Python')
+    case 'for':
+        print('is Python')
+    case 'while':
+        print('is Python')
+    case 'break':
+        print('is Python')
+    case 'continue':
+        print('is Python')
+    case 'raise':
+        print('is Python')
+    case 'def':
+        print('is Python')
+    case _:
+        print('is not clear')
