99ad123ffc2904b88703d7a52d162b6e129efba3
DilshadGit <dilshad.a73@gmail.com>
2025-01-10 12:51:56 +0000

    Using while loop with and for two different number in one time increase and decrease.

---
+++ While-loop/while_10.py
@@ -0,0 +1,7 @@
+x = int(input(' Please enter a value of x: '))
+y = int(input(' Please enter a value of y: '))
+
+while x <= 10 and y >= 0:
+    x += 1
+    y -= 1
+    print('Automatic up: ', x, ' - ', 'Automatic down: ', y )
