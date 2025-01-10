2812255addb580d2bc83598382495a620fc132cc
DilshadGit <dilshad.a73@gmail.com>
2025-01-10 16:53:13 +0000

    Run for loop and use len to count a number of each str in the py_libraries.

---
+++ For-loop/len_for.py
@@ -0,0 +1,5 @@
+py_libraries = ['pandas', 'numpy', 'scipy', 'scikit-learn', 'statesmodels', 'NLTK',
+                'seaborn', 'matplotlib', 'tensorflow', 'keras']
+
+for library in range(len(py_libraries)):
+    print(library + 1, py_libraries[library])
