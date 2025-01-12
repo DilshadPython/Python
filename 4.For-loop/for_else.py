08afc3da30d84d10ee73f63b1bd99139c00d7a2a
DilshadGit <dilshad.a73@gmail.com>
2025-01-12 16:49:06 +0000

    Using else with for loop insteate of using with if statmenet only.

---
+++ 4.For-loop/for_else.py
@@ -0,0 +1,8 @@
+names = ['Python', 'Java', 'C++', 'Django', 'JavaScript']
+
+for name in names:
+    if name == 'DotNet':
+        print('DotNet is found')
+        break
+else:
+    print('DotNet not found')
