b5ab439970bdcb9ed712a2dec15d595815200beb
DilshadGit <dilshad.a73@gmail.com>
2025-01-14 23:39:34 +0000

    We are using sys with argv together in the file when the user run python sys_argv.py if the value in sys.argv[0] doesn't need any name or word but if sys.argv[1] or sys.argv[2] we have to run this way python sys_argv.py Python and for sys.argv[2] run python sys_argv.py Java Python to see the layout of the print.

---
+++ Sys/sys_argv.py
@@ -0,0 +1,21 @@
+import sys
+
+# the file name is sys_arvg.py
+# run python sys_argv.py
+print('Welcome to tutorials of ', sys.argv[0])
+
+# Whe you run this file you have to run python sys_argv.py Java or Python
+print('Welcome to tutorials of ', sys.argv[1])
+
+# Whe you run this file you have to run python sys_argv.py Java and Python
+print('Welcome to tutorials of ', sys.argv[2])
+
+#Outcome
+'''
+python sys_argv.py Java Python
+
+Welcome to tutorials of  sys_argv.py
+Welcome to tutorials of  Java
+Welcome to tutorials of  Python
+
+'''
