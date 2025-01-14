fa2a8d3ce02bba221ed08f88f425afcfa6aa682c
DilshadGit <dilshad.a73@gmail.com>
2025-01-14 23:41:21 +0000

    We are using sys with argv together in the file when the user run python sys_argv.py if the value in sys.argv[0] doesn't need any name or word but if sys.argv[1] or sys.argv[2] we have to run this way python sys_argv.py Python and for sys.argv[2] run python sys_argv.py Java Python to see the layout of the print.

--- Sys/sys_argv.py
+++ Sys/sys_argv.py
@@ -1,4 +1,10 @@
 import sys
+'''
+We are using sys with argv together in the file when the user run python sys_argv.py
+if the value in sys.argv[0] doesn't need any name or word but if sys.argv[1] we have 
+to run this way python sys_argv.py Python 
+and for sys.argv[2] run python sys_argv.py Java Python to see the layout of the print.
+'''
 
 # the file name is sys_arvg.py
 # run python sys_argv.py
