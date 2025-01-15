a88fb2c05cae2ea3c0357a40d9823c17a5e27b61
DilshadGit <dilshad.a73@gmail.com>
2025-01-15 00:45:43 +0000

    Using for loop and how many arguments given will loop throw from no argument to the number of the arguments we enter.

--- Sys/sys_for.py
+++ Sys/sys_for.py
@@ -14,6 +14,5 @@ Welcome to  Flask  tutorials
 if len(sys.argv) < 2:
     sys.exit('The file run with less argument')
 
-# removed else and use sys.exit to get out of display IndexError
 for arg in sys.argv:
     print('Welcome to ', arg, ' tutorials')
