e1b7cfa8fc366b5795a38b67a218586ebed6ddb1
DilshadGit <dilshad.a73@gmail.com>
2025-01-20 11:40:30 +0000

    Update with some info.

--- Api/find_name.py
+++ Api/find_name.py
@@ -6,7 +6,7 @@ if len(sys.argv) != 2:
     sys.exit("Usage: python search_song.py song_name")
 
 # this is apple library link https://itunes.apple.com/search?entity=song&limit=20&term=love
-# searching for 25 song have the love word return with trackname results
+# searching for 25 song have the love word return with trackname results, limit=25
 display = requests.get('https://itunes.apple.com/search?entity=song&limit=25&term=' + (sys.argv[1]))
 
 # Now we are search for the trackName in the library because love song has other name check to find
