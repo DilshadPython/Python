99e2619be3c7068c9ec7677dbb8a64247cbd4b42
DilshadGit <dilshad.a73@gmail.com>
2025-01-21 20:25:20 +0000

    Read from the file and sorted before printed.

--- Fio/sorted_in_file.py
+++ Fio/sorted_in_file.py
@@ -1,6 +1,4 @@
+# read from the file and sorted before print in the screen
 with open('cities.txt') as f:
     for line in sorted(f):
         print('Sorted in ', line.strip())
-
-
-for city in sorted()
