86a312c3466e549f7b40c07e27e456e4ea395ba9
DilshadGit <dilshad.a73@gmail.com>
2025-01-24 11:15:36 +0000

    Explain slicing how it works.

---
+++ Slicing/example.py
@@ -0,0 +1,9 @@
+numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# slicing is [start:end:step]
+print(numbers[1:8])
+print(numbers[::10])
+# start from 2, ended in 10, and each step is 2
+print(numbers[2:10:2])
+# start from 0 to the end and 3 steps
+print(numbers[0::3])
+print(numbers[::-1])
