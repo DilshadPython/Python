201dcae332ddb1cb80e0e2039723f5e3b6e23d8f
DilshadGit <dilshad.a73@gmail.com>
2025-02-03 11:09:55 +0000

    Update spelling.

--- Format/format.py
+++ Format/format.py
@@ -7,7 +7,7 @@ print(location)
 
 pos = (40, 175, '01/03/1975')
 
-test = 'Dilshad is a={pos[0]} years old, he is b={pos[1]} cm tall and he is born at c={pos[2]}.'.format(
+test = 'Dilshad is a={pos[0]} years old, he is b={pos[1]} cm tall and date of birth  c={pos[2]}.'.format(
     pos=pos)
 
 print(test)
