084e7f459cf7487b289d1f679f87ea00a3809a43
DilshadGit <dilshad.a73@gmail.com>
2025-02-04 10:34:49 +0000

    Expand index in different way.

--- List-comprehensions/index_list.py
+++ List-comprehensions/index_list.py
@@ -3,3 +3,16 @@ users = ['John', 'Mike', 'Cludia', 'George', 'Kim', 'Elena']
 for user in range(len(users)):
     print(user + 1, users[user])
 
+print()
+# Now we will sorted the user alphabetically with number
+for user in sorted(users):
+    print(user)
+
+print()
+for user in range(len(users)):
+    print(user + 1, sorted(users[user]))
+
+print()
+# print how many character each name in the list
+for user in range(len(users)):
+    print(len(users[user]), ' = ', users[user])
