418d90b6dda6dec4767c5dd9c86e4895d640e8f9
DilshadGit <dilshad.a73@gmail.com>
2025-02-04 13:57:55 +0000

    Need to explain the different between n: int or n: str and int() or str().

---
+++ Strings/_:str.py
@@ -0,0 +1,9 @@
+name: str = '<NAME>'
+print(name)
+
+print('####################################')
+
+name: str = input('What is your name? ')
+print(name)
+print(type(name))
+print(type(input('')))
