e63e4a0940441268e99b0934eadfaf122d3b470e
DilshadGit <Dilshad.a73@gmail.com>
2025-04-05 22:06:32 +0100

    create tuple as an array.

--- NumPy/create-array/create_array.py
+++ NumPy/create-array/create_array.py
@@ -26,3 +26,14 @@ print(three_d_array.shape)
 
 print('\nShow data-type:')
 print(three_d_array.dtype)
+
+tuple_list = (17, 9, 76, 1, 2, 3, 4, 17, 14, 5, 8, 54, 4, 12, 3, 17, 1, 9, 17)
+new_array = np.array(tuple_list)
+print(new_array)
+# now we changing all
+
+# In set all duplication will removed not display
+print("\nIn set all duplication will removed not display")
+set_list = {17, 9, 76, 1, 2, 3, 9, 4, 5, 8, 54, 4, 12, 3, 17, 1, 22}
+myset = np.array(set_list)
+print(myset)
