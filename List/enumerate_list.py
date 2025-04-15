e7f551ffed05119594f8af573e7f62fed5836c64
DilshadGit <Dilshad.a73@gmail.com>
2025-04-15 23:02:52 +0100

    Start linst from one not zero using start=1 .

--- List/enumerate_list.py
+++ List/enumerate_list.py
@@ -1,3 +1,11 @@
 
 for (x, y) in enumerate(["London", "Berlin", "New Yourk", "Paris", 'Rome', 'Oslo', ['a', 'b']]):
     print(x, y)
+print('\n------\n')
+cities = ["London", "Berlin", "New Yourk", "Paris", 'Rome', 'Oslo']
+
+for index, city in enumerate(cities):
+    print(index, city)
+print('\n------\n')
+for index, city in enumerate(cities, start=1):
+    print(index, city)
