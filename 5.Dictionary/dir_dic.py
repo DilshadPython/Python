78ad4664f4401c7c1c38860716359c3736d64142
DilshadGit <Dilshad.a73@gmail.com>
2025-04-16 22:19:51 +0100

    How to find the methods of dictionary and how to using it.

--- 5.Dictionary/dir_dic.py
+++ 5.Dictionary/dir_dic.py
@@ -1,4 +1,4 @@
-city = {'name':'Berlin','population':3.28, 'county': 'Germany'}
+city = {'name':'Berlin','population':3.28, 'country': 'Germany', 'capital': True}
 print(dir(city))
 
 print(city.keys())
@@ -18,16 +18,22 @@ update()
 values()
 '''
 # how to use it:
-city.clear()
+
 new_city = city.copy()
 the_city = dict.fromkeys(['name','population','county'])
 print(the_city)
 city.get('name')
 city.items()
 city.keys()
-city.pop('name')
+city.pop('capital')
+print(city)
 city.popitem()
 city.setdefault('name','Berlin')
 city.update({'name':'Berlin','population':3.28, 'county': 'Germany'})
 city.values()
 
+del city['population']
+print(city)
+
+city.clear()
+print(city)
