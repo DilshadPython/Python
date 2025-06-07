ee51c46764c4078526fc6333b2b279e9b345eadd
DilshadGit <Dilshad.a73@gmail.com>
2025-06-07 22:31:49 +0100

    create list with duplicate num add them together after display as set which remove all duplicated number.

--- 4.For-loop/add_nums.py
+++ 4.For-loop/add_nums.py
@@ -1,18 +1,19 @@
 
 nums = [4, 5, 7, 5, 4, 8]
 total = 0
-# for num in nums:
-#     total = sum(num)
-#     print(total)
+for num in nums:
+    total = num + total
+    print(total, ' << This is for loops')
 
 total = sum(nums)
-print(total)
-print('======\n')
+print('\n Totals: ', total, ' << ')
+print('\n==================')
 
 output = set()
 
 for num in nums:
-    print(num)
-print('********\n')
+    print('\n\t', num, '\t' )
+print('******************\n')
+# set remove all duplicated number s in the list
 x = set(nums)
-print('Set remove all duplicates number in the nums list and change the list to set: ', x, '\n')
+print('Set remove all duplicates number in the nums list and change the list to set: \n', x, '\n')
