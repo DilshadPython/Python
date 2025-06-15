ba268820392bba0e0e3355bb6381aa7d0cbeea91
DilshadGit <Dilshad.a73@gmail.com>
2025-06-15 21:45:33 +0100

    Display couple string together and some numbers together as calculation.

---
+++ 6.Strings/add_str_together.py
@@ -0,0 +1,15 @@
+a = 'Hello '
+b = 'Python '
+c = 'Language'
+
+print(a + b + c)
+
+y = 33
+z = 81
+
+print(y + z)
+
+print('Add number with str has to use , not + :')
+number = 1973
+name = 'Dilshad'
+print(number, name)
