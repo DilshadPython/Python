26c256c6d2ade77822f9ebd6282f7fc02541def4
DilshadGit <Dilshad.a73@gmail.com>
2025-06-15 21:10:32 +0100

    create var with multiple name as string or number and list with tuple to showing how it works.

---
+++ 6.Strings/multi_var.py
@@ -0,0 +1,34 @@
+a, b, c, d = 7, 4, 19, 0
+
+print(a, b, c, d)
+print('\n#############')
+one, two, three = 'Linux', 'Apple', 'Windows'
+
+print(one, two, three)
+print('\n=============')
+print('one: ', one)
+print(two)
+print(three)
+
+print('\n=============')
+
+a = b = c = 'Hellow world'
+print('a: ', a)
+print('b: ', b)
+print('c: ', c)
+
+print('\n##############')
+
+text = ['Linux', 'Apple', 'Windows']
+m, n, o = text
+print('m: ', m)
+print('n: ', n)
+print('o: ', o)
+
+print('\n=============')
+
+os = ('Linux', 'Apple', 'Windows')
+x, y, z = os
+print('x: ', x)
+print('y: ', y)
+print('z: ', z)
