e7edf77bea69bf0b88bcfa05259adc0eff4d5a0a
DilshadGit <Dilshad.a73@gmail.com>
2025-06-15 22:41:51 +0100

    Update float number and display the types of every var.

--- 2.Numbers/floats.py
+++ 2.Numbers/floats.py
@@ -1,33 +1,49 @@
-899num1 = float(input('Enter num1: '))
+num1 = float(input('Enter num1: '))
 num2 = float(input('Enter num2: '))
 
-print('Sum ', num1 + num2)
-print('Sub ', num1 - num2)
-print('Multi ', num1 * num2)
-print('Divid ', num1 / num2)
-print('Division with no reminder ', num1 // num2)
-print('Power ', num1 ** num2)
-print('Moduls reminder', num1 % num2)
+print('Sum: ', num1 + num2)
+print('Sub: ', num1 - num2)
+print('Multi: ', num1 * num2)
+print('Divid: ', num1 / num2)
+print('Division with no reminder: ', num1 // num2)
+print('Power: ', num1 ** num2)
+print('Moduls reminder: ', num1 % num2)
 
 print('\n===========================')
 
 a = 3e8 # this bring 8 time 0 to front of 3 and change to float
-b = float(9)
-c = float('2548')
-d = float('nan')
-e = float('inf')
-f = float('-inf')
-
-x = 5e3
-y = 12e9
-z = 348e4
-
 print(a)
+print(type(a))
+
+b = float(9)
 print(b)
+print(type(b))
+
+c = float('2548')
 print(c)
+print(type(c))
+
+d = float('nan')
 print(d)
+print(type(d))
+
+e = float('inf')
 print(e)
+print(type(e))
+
+f = float('-inf')
 print(f)
+print(type(f))
+
+print('\n===========')
+x = 5e3
 print(x)
+print(type(x))
+y = 12e9
 print(y)
+print(type(y))
+z = 348e4
 print(z)
+print(type(z))
+
+
