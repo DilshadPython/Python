fd24c9103a09d710786cff88a876cd7aff449bdf
DilshadGit <Dilshad.a73@gmail.com>
2025-06-15 21:55:40 +0100

    Define var inside and outside the function and see how it work if define the var inside the function again.'

--- 6.Strings/global_var.py
+++ 6.Strings/global_var.py
@@ -5,6 +5,16 @@ def myname():
 
 myname()
 
+first_name = 'Tomas'
+
+def myfirstname():
+    # here the func don't display Tomas and Julia because no print and no return only define
+    # first name again with different name
+    first_name = 'Julia'
+
+myfirstname()
+print('Your first name is  ' + first_name)
+
 
 year = 1978
 
@@ -14,6 +24,7 @@ def myage():
     print('My age is ' + str(year))
 
 myage()
+print('But my age here is ' + str(year))
 
 language = 'Python'
 
