027d0c64ac00e71b179f15889fc0110174f05449
DilshadGit <Dilshad.a73@gmail.com>
2025-06-18 19:10:19 +0100

    How all operators working in python one by one.

---
+++ 7.Operators/operators.py
@@ -0,0 +1,88 @@
+x = 8
+print(x)
+print('=========\n')
+
+print('========= : += \n')
+a = 7
+a += 5
+# a = a + 5
+print(a)
+
+
+print('========= : -= \n')
+b = -9
+b -= 10
+# b = b - 10
+print(b)
+
+print('========= : -= \n')
+c = 76
+c -= 26
+# c = c - 26
+print(c)
+
+print('========= : *= \n')
+d = 9
+d *= 8
+# d = d * 8
+print(d)
+
+print('========= : /= \n')
+e = 81
+e /= 14
+# e = e / 14
+print(e)
+
+print('========= :  %= \n')
+f = 9
+f %= 2
+# f = 9 % 2
+print(f)
+
+print('========= : //= \n')
+g = 21
+g //= 7
+# g = g // 7
+print(g)
+
+print('========= : **= \n')
+h = 5
+h **= 3
+# h = h ** 3
+print(h)
+
+print('========= : &= \n')
+i = 16
+i &= 4
+# i = i & 4
+print(i)
+
+print('========= : |= \n')
+j = 8
+j |= 4
+print(j)
+
+print('========= : ^= \n')
+k = 5
+k ^= 4
+print(k)
+
+print('========= : <<= \n')
+l = 5
+l <<= 2
+print(l)
+
+print('========= : >>= \n')
+m = 5
+m >>= 2
+print(m)
+
+print('\n========= : &>> \n')
+n = 5
+n &= 4
+print(n)
+
+print('=========\n')
+
+print('\n========= : := \n')
+print(o := 9)
