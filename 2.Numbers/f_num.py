06c4edf8b1c0f54a1c9a1079bb584df911e828d3
DilshadGit <Dilshad.a73@gmail.com>
2025-06-18 12:21:44 +0100

    Move this file from string t the number.

---
+++ 2.Numbers/f_num.py
@@ -0,0 +1,11 @@
+
+numbers = [2, 3, 9, 24]
+
+addnum = 3
+
+for num in numbers:
+    print(f'{num}', ' + ',  f'{addnum} = ')
+    num += addnum
+    print(f'{num} ')
+
+print(f'All this {num} in the list of {numbers}  added by ' + f'{addnum}')
