fc4421880e03d41342cf96d490b2ae7a92a4b3fd
DilshadGit <Dilshad.a73@gmail.com>
2025-06-18 13:01:02 +0100

    The escape character allows you to use double quotes when you normally would not be allowed.

---
+++ 6.Strings/escape_char.py
@@ -0,0 +1,7 @@
+msg = 'To day is "Friday" is very cold but yesterday was "Thursday" very nice weather.'
+print(msg)
+print('=====================\n')
+
+# The escape character allows you to use double quotes when you normally would not be allowed:
+msg = 'To day is \'Friday\' is very cold but yesterday was \'Thursday\' very nice weather.'
+print(msg)
