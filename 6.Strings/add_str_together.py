dfbadfb49b778152f6713669654c1537fb799eaf
DilshadGit <Dilshad.a73@gmail.com>
2025-06-18 12:44:12 +0100

    Add a space between the words and merge to d this is called concatenate

--- 6.Strings/add_str_together.py
+++ 6.Strings/add_str_together.py
@@ -1,8 +1,14 @@
-a = 'Hello '
-b = 'Python '
+a = 'Hello'
+b = 'Python'
 c = 'Language'
 
+
 print(a + b + c)
+print('=========\n')
+
+# Add a space between the words and merge to d this is called concatenate
+d = a + ' ' + b + ' ' + c
+print(d)
 
 y = 33
 z = 81
@@ -13,3 +19,4 @@ print('Add number with str has to use , not + :')
 number = 1973
 name = 'Dilshad'
 print(number, name)
+
