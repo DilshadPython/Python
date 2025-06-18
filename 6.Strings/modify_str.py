6f521561daa0048310cb8dfe787c4dcfb1e625e2
DilshadGit <Dilshad.a73@gmail.com>
2025-06-18 12:38:58 +0100

    Create a message as string and using some methods in the string to shows how it works form examle display all string as uupercase using upper() methods or lowercase using lower() method, remove all whitespace from beginning and end of the message replace some char from lowercase to uppercase use replace() method and change the words from string to words in the lisy using split(0 method.

---
+++ 6.Strings/modify_str.py
@@ -0,0 +1,22 @@
+message = ' Welcome to the Python '
+
+print(message)
+print('========\n')
+
+print(message.upper())
+print('========\n')
+
+print(message.lower())
+print('========\n')
+
+print('Remove all white spaces from beginning and end of string using strip() method')
+print(message.strip())
+print('========\n')
+
+print('replace all e to E')
+print(message.replace('e', 'E'))
+print('========\n')
+
+print('The split() method splits a string into a list of words')
+print(message.split())
+print('========\n')
