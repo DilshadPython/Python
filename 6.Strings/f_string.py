1473dcbdf33b13a756a551dfae539de68545584e
DilshadGit <Dilshad.a73@gmail.com>
2025-06-18 12:50:34 +0100

    How to use f string in the string and how add number like {37} to the text using f string .

--- 6.Strings/f_string.py
+++ 6.Strings/f_string.py
@@ -18,5 +18,10 @@ my_name = 'Azad'
 print()
 print(f"Hello, {my_name.upper()}")
 
+age = 37
+txt = f'My name is {my_name} and I am {age} years old'
+print(txt)
+
+
 print("\n Display all methods use with string: \n")
 print(dir(my_name))
