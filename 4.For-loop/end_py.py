27e9f02e343cb592270d597971705663eec7aa48
DilshadGit <Dilshad.a73@gmail.com>
2025-06-19 11:22:22 +0100

    How end works in python.

---
+++ 4.For-loop/end_py.py
@@ -0,0 +1,14 @@
+# end in python display the result horizontally and end it when reach to the point
+for i in range(16):
+    print(i, end=", ")
+
+print('=======================\n')
+print('Welcome to')
+print('Python')
+
+print('Hi', end=' ')
+print('JavaScript')
+
+for i in range(10):
+    print(i, end=" & ")
+    continue
