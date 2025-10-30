f4182ccb85d5265275e7fef2a2d28aee0c4cbc32
DilshadGit <dilshad.a73@gmail.com>
2025-10-30 14:46:27 +0000

    Create folder with defile the text filder inside created folder and read all names in the text file.

--- Fio/list_city.py
+++ Fio/list_city.py
@@ -1,4 +1,7 @@
-cities = str(input("Enter the cities you want: "))
+import os
+
+from pathlib import Path
+
 '''
 "r" - Read - Default value. Opens a file for reading, error if the file does not exist
 "a" - Append - Opens a file for appending, creates the file if it does not exist
@@ -11,6 +14,31 @@ In addition you can specify if the file should be handled as binary or text mode
 
 '''
 
-# When you use with automatically open and  closed
-with open('cities.txt', 'a') as f:
-    f.write(f'{cities}\n')
+# create the folder
+create_dir = Path('TextFolder')
+
+# defile the path of text file inside the folder
+txt_file = Path(create_dir, "cities.txt")
+
+with open(txt_file, 'r') as f:
+    for line in f:
+        print('Hello,', line.rstrip())
+
+
+# file = open(test_folder / 'cities.txt', 'r')
+
+
+# with open(enter_folder(file, 'r')) as f:
+#     content = f.read()
+#     print(content)
+
+# file.close()
+
+# cities = str(input("Enter the cities you want: "))
+
+# read_cities = open(test_folder, 'r')
+
+# # When you use with automatically open and  closed
+# with open(enter_folder, 'r') as f:
+# with open('cities.txt', 'a') as f:
+#     f.write(f'{cities}\n')
