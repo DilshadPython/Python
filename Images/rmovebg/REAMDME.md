7c569348ce38f555d3f3a38737ba50fe02edbc86
DilshadGit <dilshad.a73@gmail.com>
2025-11-20 00:19:59 +0000

    Update the image to another to test again because of the version of package may need an update in the future but still working until today.

---
+++ Images/rmovebg/REAMDME.md
@@ -0,0 +1,7 @@
+# create venv
+
+install all requirements in requirements.txt
+
+pip install -r requirements.txt
+
+python proj.py get the results which is remove the background of the image
--- Images/rmovebg/proj.py
+++ Images/rmovebg/proj.py
@@ -1,10 +1,12 @@
-from rembg import remove
 from PIL import Image
+from rembg import remove
 
-image_path = "dilshad.jpg"
-result_path = "result.png"
+# image_path = "dilshad.jpg"
+# result_path = "result.png"
+
+image_path = "ario.jpg"
+result_path = "ario_after.png"
 
 input = Image.open(image_path)
 output = remove(input)
 output.save(result_path)
-
