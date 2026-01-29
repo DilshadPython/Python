40508d4a2c60e42ad82afd428aa579b98e258d3c
DilshadPython <dilshad.abdulla@icloud.com>
2026-01-29 02:51:14 +0000

    print more in different way.

--- Slicing/example.py
+++ Slicing/example.py
@@ -1,4 +1,5 @@
 numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
 # slicing is [start:end:step]
 print(numbers[1:8])
 print(numbers[::10])
@@ -11,3 +12,6 @@ print(numbers[::-1])
 print(numbers[4:])
 ###
 print(numbers[-8:-2])
+
+##
+print(numbers[-3:2:-2])
