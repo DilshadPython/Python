30e9229f461b24bad7698882f1172e0fd0be70b8
DilshadPython <dilshad.abdulla@icloud.com>
2026-01-30 03:17:02 +0000

    Reverse string using slicing .

---
+++ Slicing/url_slici.py
@@ -0,0 +1,11 @@
+my_url = 'http://www.python.org/'
+# slicing is [start:end:step]
+print('\n ', my_url)
+print('\n---------------------\n')
+print(my_url[::-1])
+
+print('\n======================\n')
+print(my_url[-5:])
+
+print('\n======================\n')
+print(my_url[-15::-2])
