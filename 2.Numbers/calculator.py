800eacc03b8372a8f4f4bdba677bb2c6ea920564
DilshadGit <dilshad.abdulla.com>
2026-02-26 10:09:24 +0000

    Update the code.

--- 2.Numbers/calculator.py
+++ 2.Numbers/calculator.py
@@ -1,23 +1,22 @@
 print('Small calculation op means *, +, -, /, ^, //')
 
 
-def call(num1=int(input("Enter num1: ")), op=input('Enter op: '), num2=int(input("Enter num2: "))):
+def main(num1=int(input("Enter num1: ")), op=input('Enter op: '), num2=int(input("Enter num2: "))):
+    result = None
     if op == '+':
-        result = num1 + num2
+        print(num1 + num2)
     elif op == '-':
-        result = num1 - num2
+        print(num1 - num2)
     elif op == '*':
-        result = num1 * num2
+        print(num1 * num2)
     elif op == '/':
-        result = num1 / num2
+        print(num1 / num2)
     elif op == '^':
-        result = num1 ** num2
+        print(num1 ** num2)
     elif op == '%':
-        result = num1 % num2
+        print(num1 % num2)
     elif op == '//':
-        result = num1 // num2
+        print(num1 // num2)
 
-    return print(result)
-
-
-call()
+if __name__ == '__main__':
+    main()
