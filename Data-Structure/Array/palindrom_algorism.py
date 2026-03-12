df90984bbd9e04dfaf48c47ef24a609db524dc0e
DilshadGit <dilshad.abdulla@icloud.com>
2026-03-12 14:33:39 +0000

    Create parindome which is read in both way the same like forword and backword, example radar, madam, dad, 101, 404.

---
+++ Data-Structure/Array/palindrom_algorism.py
@@ -0,0 +1,19 @@
+'''
+What is Palindrome
+It's a word or a number when you read the same in both way forward and backword
+like madam, mom, dad, 404, 101
+'''
+def palindrom(word):
+    if word == word[::-1]:
+        print(word)
+        return True
+    print(word)
+    return False
+
+
+if __name__ == '__main__':
+    print(palindrom('madam'))
+    # or
+    # print(palindrom('hello'))
+
+
