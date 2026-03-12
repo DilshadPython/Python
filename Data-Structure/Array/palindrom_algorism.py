7dd10280bab1fd08f830b3b3501fca37eb4966a4
DilshadGit <dilshad.abdulla@icloud.com>
2026-03-12 15:55:40 +0000

    add some notice.

--- Data-Structure/Array/palindrom_algorism.py
+++ Data-Structure/Array/palindrom_algorism.py
@@ -1,7 +1,7 @@
 '''
 What is Palindrome
 It's a word or a number when you read the same in both way forward and backword
-like madam, mom, dad, 404, 101
+like madam, mom, dad, 404, 101, radar
 '''
 def palindrom(word):
     if word == word[::-1]:
