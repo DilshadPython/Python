5f7acbcd8567c67a14b3442bd97dd530e6d33e55
DilshadGit <dilshad.abdulla@icloud.com>
2026-03-21 16:54:04 +0000

    create a list with some multi integer after define max and min number to 0, when we search all numbers display the maximum number and minimum number from the list.

--- Data-Structure/Array/max_min_average_array.py
+++ Data-Structure/Array/max_min_average_array.py
@@ -1,10 +1,10 @@
-#The index 0  1	 2  3	4	5  6   7   8  9   10
-numbers = [1, 5, 7, -2, 10, 17, 27, 39, 8, 11, -3]
+# The index start:  0  1	 2  3	4	5  6   7   8  9   10
+# from _pyrepl import __main__ or you can use if __name__ == '__main__':
+numbers = [1, 5, 7, -2, 10, 17, 27, 39, 8, 11, -3, -6]
 
 print()
 print('We will display all items except the last 3 index: ')
 print(numbers[0:-3])
-
 
 print()
 print('We will search for the largest numbers, this is call liner search:')
@@ -17,7 +17,7 @@ for num in numbers:
     elif num < minimum:
         minimum = num
 
-
-print('The maximum number is: ', maximum)
-print()
-print('The minimum number is: ', minimum)
+if __name__ == "__main__":
+    print('The maximum number is: ', maximum)
+    print()
+    print('The minimum number is: ', minimum)
