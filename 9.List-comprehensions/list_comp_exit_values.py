8072c6b401e4804f9318b75133be0a06201c0d79
DilshadGit <dilshad.abdulla@icloud.com>
2026-05-17 15:18:03 +0100

    Adding some extra func can used with list and display how it work, like copy, sort, pop, insert, len etc ..

--- 9.List-comprehensions/list_comp_exit_values.py
+++ 9.List-comprehensions/list_comp_exit_values.py
@@ -12,3 +12,34 @@ print(nums)
 
 nums.reverse()
 print(nums)
+
+print('Copy the list: ')
+nums.copy()
+print(nums)
+
+print('Clear the list: ')
+nums.clear()
+print(nums)
+
+nums = [74, 91, 53, 2, 70, 36, 5, 55, 31, 54, -1, 8, 0]
+print('Length of the list: ')
+print(len(nums))
+
+print('Display the list: ')
+print(nums)
+
+print('Remove the number 8: ')
+nums.remove(8)
+print(nums)
+
+print('Append and pop the number 100: ')
+nums.append(100)
+print(nums)
+
+print('Pop the last number: ')
+nums.pop()
+print(nums)
+
+print('Insert the number 100 at the index 3: ')
+nums.insert(3, 200)
+print(nums)
