143c609f9cc8a11fe99b3d5daafe5f72359055c5
DilshadGit <dilshad.abdulla@icloud.com>
2026-05-17 16:10:23 +0100

    Create a method that swap the index of list contain random number and entered manually, the length of list less than 10 numbers. The concept is do not use reverse() function created by python.

--- Data-Structure/Array/swap_index.py
+++ Data-Structure/Array/swap_index.py
@@ -1,4 +1,20 @@
-def reverse_index(numbers):
+'''
+Create a method that swap the index of list contain random number and entered manually,
+ the length of list less than 10 numbers. The concept is do not use reverse()
+ function created by python.
+Example:
+Enter number 1: 25
+Enter number 2: 14
+Enter number 3: 3
+Enter number 4: 6
+Enter number 5: 87
+Created List: [25, 14, 3, 6, 87]
+
+Swapped List: [87, 6, 3, 14, 25]
+'''
+import random
+
+def swap_index(numbers):
 
     # define the first item
     start_index = 0
@@ -11,9 +27,15 @@ def reverse_index(numbers):
         start_index += 1
         end_index -= 1
 
-if __name__ == '__main__':
-    numbers = [1, 2, 3, -4, 5, -6, 7, 8, -9, 10]
-    print(numbers, '\n')
-    reverse_index(numbers)
-    print(numbers)
+# Random length less than 10
+length = random.randint(0, 10)
+numbers = []
 
+for i in range(length):
+    num = int(input(f"Enter number {i+1}: "))
+    numbers.append(num)
+
+if __name__ == '__main__':
+    print("Created List:", numbers,'\n')
+    swap_index(numbers)
+    print("Swapped List: ", numbers)
